dcb535d02b7fa655f70649e13cdc74a27767bed9
akildevs1000 <107389124+akildevs1000@users.noreply.github.com>
2022-09-23 18:46:56 +0400

    testing

--- backend/routes/test.php
+++ backend/routes/test.php
@@ -4,10 +4,50 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Storage;
 
-Route::post('/do_spaces',function(Request $request){
-    return $request->file("file")->storePublicly("upload","do") ? 1 : "0";
+Route::post('/do_spaces', function (Request $request) {
+    return $request->file("file")->storePublicly("upload", "do") ? 1 : "0";
 });
 
-Route::post('/log_payload',function(Request $request){
+Route::post('/log_payload', function (Request $request) {
     return $request->all();
+});
+
+Route::get('/php_mail', function (Request $request) {
+    // ini_set('SMTP', "server.com");
+    // ini_set('smtp_port', "25");
+    // ini_set('sendmail_from', "email@domain.com");
+
+
+    $to = env("RECIPIENT_LIST");
+    $subject = "HTML email";
+
+    $message = "
+                <html>
+                <head>
+                <title>HTML email</title>
+                </head>
+                <body>
+                <p>This email contains HTML Tags!</p>
+                <table>
+                <tr>
+                <th>Firstname</th>
+                <th>Lastname</th>
+                </tr>
+                <tr>
+                <td>John</td>
+                <td>Doe</td>
+                </tr>
+                </table>
+                </body>
+                </html>
+                ";
+
+    // Always set content-type when sending HTML email
+    $headers = "MIME-Version: 1.0" . "\r\n";
+    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+    // More headers
+    $headers .= 'From: <akildevs1000@gmail.com>' . "\r\n";
+
+    return mail($to, $subject, $message, $headers);
 });
