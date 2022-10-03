909e192fd6ad6124cb337c48258c894f7e4dc6ec
akildevs1000 <107389124+akildevs1000@users.noreply.github.com>
2022-10-03 11:15:22 +0400

    increase department limit character

--- backend/app/Http/Requests/Department/DepartmentRequest.php
+++ backend/app/Http/Requests/Department/DepartmentRequest.php
@@ -26,7 +26,7 @@ class DepartmentRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => 'required|min:4|max:8'
+            'name' => 'required|min:4|max:20'
         ];
     }
 }
