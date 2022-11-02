afce64ced4d670bde0d6821e205d065e902162f7
akildevs1000 <107389124+akildevs1000@users.noreply.github.com>
2022-11-02 12:21:12 +0400

    fixed

--- backend/app/Http/Controllers/Reports/WeeklyController.php
+++ backend/app/Http/Controllers/Reports/WeeklyController.php
@@ -366,7 +366,7 @@ class WeeklyController extends Controller
                 $work_minutes += $work_minute;
             }
 
-            if ($ot != '---') {
+            if ($ot != '---' && $ot != 'NA') {
                 list($ot_hour, $ot_minute) = explode(':', $ot);
                 $ot_minutes += $ot_hour * 60;
                 $ot_minutes += $ot_minute;
