9171781bfc28a561a90b44ac10848a2e202a185b
akildevs1000 <107389124+akildevs1000@users.noreply.github.com>
2023-03-02 12:08:38 +0400

    for staging

--- frontend/worker.js
+++ frontend/worker.js
@@ -1,8 +1,20 @@
 const WebSocket = require("ws");
 require("dotenv").config();
 const axios = require("axios");
-
 const fs = require("fs");
+
+let { Client } = require("pg");
+
+const client = new Client({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  port: process.env.DB_PORT,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
+});
+
+client.connect();
+
 let socket = new WebSocket(process.env.SOCKET_ENDPOINT);
 socket.onopen = () => console.log("connected\n");
 socket.onerror = () => console.log("error\n");
@@ -15,22 +27,31 @@ socket.onmessage = ({ data }) => {
     RecordNumber: SerialNumber
   } = JSON.parse(data).Data;
 
-  if (UserID !== 0) {
-    let str = `${UserID},${DeviceID},${LogTime.replace(
-      "T",
-      " "
-    )},${SerialNumber}`;
+  LogTime = LogTime.replace("T", " ");
 
-    let payload = {
-      UserID: UserID,
-      DeviceID: DeviceID,
-      LogTime: LogTime
-    };
+  if (UserID > 0) {
+    let str = `${UserID},${DeviceID},${LogTime},${SerialNumber}`;
 
-    fs.appendFileSync("/var/www/ideahrms/backend/logs/logs.csv", str + "\n");
-    fs.appendFileSync(
-      "/var/www/staging/ideahrms/backend/logs/logs.csv",
-      str + "\n"
+    console.log(str + "\n");
+
+    client.query(
+      `INSERT INTO public.attendance_logs("UserID", "LogTime", "DeviceID", "SerialNumber",checked)
+      VALUES ('${UserID}', '${LogTime}', '${DeviceID}', '${SerialNumber}', '${false}')`,
+      (err, result) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        console.log("Data Insert. " + str);
+      }
     );
+
+    // fs.appendFileSync("logs.csv", str + "\n");
+
+    // fs.appendFileSync("/var/www/ideahrms/backend/logs/logs.csv", str + "\n");
+    // fs.appendFileSync(
+    //   "/var/www/staging/ideahrms/backend/logs/logs.csv",
+    //   str + "\n"
+    // );
   }
 };
