4243e82226dd0fe1c90a3e57d9c152461e01fbb6
akildevs1002 <akildevs1002@gmail.com>
2023-05-09 11:59:56 +0400

    restart sdk (4)

---
+++ backend/app/Console/Commands/RestartSdk.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class RestartSdk extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'restart_sdk';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'to restart sdk';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+
+        // info('restart sdk');
+        echo exec('pm2 restart 4');
+
+        // return 0;
+    }
+}
