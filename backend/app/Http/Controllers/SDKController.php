e3f11207f869e3508ac01d41d6c1deb8423738f2
akildevs1002 <akildevs1002@gmail.com>
2023-07-08 15:04:31 +0400

    changes

--- backend/app/Http/Controllers/SDKController.php
+++ backend/app/Http/Controllers/SDKController.php
@@ -152,6 +152,8 @@ class SDKController extends Controller
           }';
         // $emailJobs = new TimezonePhotoUploadJob();
         // $this->dispatch($emailJobs);
+
+        return $data;
         $data = json_decode($data, true);
         $return = TimezonePhotoUploadJob::dispatch($data);
         // echo exec("php artisan backup:run --only-db");
