ac32b94a1182f35b622b868db6ceb20b1ec3aca5
akildevs1000 <107389124+akildevs1000@users.noreply.github.com>
2023-07-19 18:23:52 +0400

    added Avatar for first 2 letter in default

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,35 +1,65 @@
 <template>
   <v-app>
-    <v-navigation-drawer v-model="drawer" dark :mini-variant="miniVariant" :clipped="clipped" fixed app
-      :color="sideBarcolor" :style="miniVariant ? 'width: 60px' : ''" @transitionend="collapseSubItems">
+    <v-navigation-drawer
+      v-model="drawer"
+      dark
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      fixed
+      app
+      :color="sideBarcolor"
+      :style="miniVariant ? 'width: 60px' : ''"
+      @transitionend="collapseSubItems"
+    >
       <br />
       <v-list v-for="(i, idx) in items" :key="idx" style="padding: 5px 0 0 0px">
-        <v-list-item :to="i.to" router v-if="!i.hasChildren" :class="!miniVariant || 'pl-2'">
+        <v-list-item
+          :to="i.to"
+          router
+          v-if="!i.hasChildren"
+          :class="!miniVariant || 'pl-2'"
+        >
           <v-list-item-icon class="ma-2">
             <v-icon>{{ i.icon }}</v-icon>
           </v-list-item-icon>
           <v-list-item-title> {{ i.title }}&nbsp; </v-list-item-title>
         </v-list-item>
-        <v-list-item v-else :class="!miniVariant || 'pl-2'" @click="i.open_menu = !i.open_menu">
+        <v-list-item
+          v-else
+          :class="!miniVariant || 'pl-2'"
+          @click="i.open_menu = !i.open_menu"
+        >
           <v-list-item-icon class="ma-2">
             <v-icon>{{ i.icon }}</v-icon>
-            <v-icon v-if="miniVariant" small>{{ !i.open_menu ? "mdi-chevron-down" : "mdi-chevron-up" }}
+            <v-icon v-if="miniVariant" small
+              >{{ !i.open_menu ? "mdi-chevron-down" : "mdi-chevron-up" }}
             </v-icon>
           </v-list-item-icon>
 
           <v-list-item-title>{{ i.title }} </v-list-item-title>
-          <v-icon small>{{ !i.open_menu ? "mdi-chevron-down" : "mdi-chevron-up" }}
+          <v-icon small
+            >{{ !i.open_menu ? "mdi-chevron-down" : "mdi-chevron-up" }}
           </v-icon>
         </v-list-item>
         <div v-if="i.open_menu">
-          <div style="margin-left: 54px" v-for="(j, jdx) in i.hasChildren" :key="jdx">
+          <div
+            style="margin-left: 54px"
+            v-for="(j, jdx) in i.hasChildren"
+            :key="jdx"
+          >
             <!-- v-show="!miniVariant" -->
             <v-list-item style="min-height: 0" :to="j.to" class="submenutitle">
-              <v-list-item-title v-if="!miniVariant">{{ j.title }}
+              <v-list-item-title v-if="!miniVariant"
+                >{{ j.title }}
               </v-list-item-title>
 
-              <v-list-item-icon :style="miniVariant ? 'margin-left: -54px;' : ''">
-                <v-icon :to="j.to" :style="miniVariant ? 'margin-left: 12px;' : ''">
+              <v-list-item-icon
+                :style="miniVariant ? 'margin-left: -54px;' : ''"
+              >
+                <v-icon
+                  :to="j.to"
+                  :style="miniVariant ? 'margin-left: 12px;' : ''"
+                >
                   {{ j.icon }}
                 </v-icon>
               </v-list-item-icon>
@@ -43,8 +73,14 @@
     margin-left: -49px;
 " -->
 
-    <v-app-bar :color="changeColor" dark :clipped-left="clipped" fixed app
-      :style="$nuxt.$route.name == 'index' ? 'z-index: 100000' : ''">
+    <v-app-bar
+      :color="changeColor"
+      dark
+      :clipped-left="clipped"
+      fixed
+      app
+      :style="$nuxt.$route.name == 'index' ? 'z-index: 100000' : ''"
+    >
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
       <v-btn icon @click.stop="miniVariant = !miniVariant">
         <v-icon>mdi-{{ `chevron-${miniVariant ? "right" : "left"}` }}</v-icon>
@@ -55,17 +91,27 @@
       <span class="text-overflow">{{ title }}</span>
       <v-spacer></v-spacer>
 
-      <v-menu nudge-bottom="50" transition="scale-transition" origin="center center" bottom left min-width="200"
-        nudge-left="20">
+      <v-menu
+        nudge-bottom="50"
+        transition="scale-transition"
+        origin="center center"
+        bottom
+        left
+        min-width="200"
+        nudge-left="20"
+      >
         <template v-slot:activator="{ on, attrs }">
-
           <label class="px-2 text-overflow" v-bind="attrs" v-on="on">
-            {{ getUser }}
+            <!-- {{ getUser }} -->
           </label>
 
           <v-btn icon color="yellow" v-bind="attrs" v-on="on">
             <v-avatar>
-              <img :src="getLogo || '/no-image.PNG'" />
+              <v-img
+                style="width: 50px"
+                :src="`https://ui-avatars.com/api/?name=${getUser}`"
+              ></v-img>
+              <!-- <img :src="getLogo || '/no-image.PNG'" /> -->
             </v-avatar>
           </v-btn>
         </template>
@@ -77,7 +123,9 @@
                 <v-icon>mdi-account-multiple-outline</v-icon>
               </v-list-item-icon>
               <v-list-item-content>
-                <v-list-item-title class="black--text">Profile</v-list-item-title>
+                <v-list-item-title class="black--text"
+                  >Profile</v-list-item-title
+                >
               </v-list-item-content>
             </v-list-item>
 
@@ -86,7 +134,9 @@
                 <v-icon>mdi-cog</v-icon>
               </v-list-item-icon>
               <v-list-item-content>
-                <v-list-item-title class="black--text">Setting</v-list-item-title>
+                <v-list-item-title class="black--text"
+                  >Setting</v-list-item-title
+                >
               </v-list-item-content>
             </v-list-item>
 
@@ -95,13 +145,15 @@
                 <v-icon>mdi-logout</v-icon>
               </v-list-item-icon>
               <v-list-item-content>
-                <v-list-item-title class="black--text">Logout</v-list-item-title>
+                <v-list-item-title class="black--text"
+                  >Logout</v-list-item-title
+                >
               </v-list-item-content>
             </v-list-item>
           </v-list-item-group>
         </v-list>
       </v-menu>
-      <label class="px-2  ">
+      <label class="px-2">
         <!-- <v-icon v-if="pendingLeavesCount == 0">mdi mdi-bell</v-icon>
         <span v-else>
           <v-icon @click="snackNotificationText != '' && snackNotification == true" color="success">mdi
@@ -110,18 +162,30 @@
             pendingLeavesCount }}</strong></v-chip>
         </span> -->
 
-        <v-badge v-if="pendingLeavesCount > 0" @click="navigateToLeavePage()"
-          :color="pendingLeavesCount > 0 ? 'red' : 'black'" :content="pendingLeavesCount">
-          <v-icon @click="navigateToLeavePage()">mdi
-            mdi-bell-ring</v-icon>
+        <v-badge
+          v-if="pendingLeavesCount > 0"
+          @click="navigateToLeavePage()"
+          :color="pendingLeavesCount > 0 ? 'red' : 'black'"
+          :content="pendingLeavesCount"
+        >
+          <v-icon @click="navigateToLeavePage()">mdi mdi-bell-ring</v-icon>
         </v-badge>
-        <v-badge v-else @click="navigateToLeavePage()" color="black" content="0">
-          <v-icon @click="navigateToLeavePage()">mdi
-            mdi-bell-ring</v-icon>
+        <v-badge
+          v-else
+          @click="navigateToLeavePage()"
+          color="black"
+          content="0"
+        >
+          <v-icon @click="navigateToLeavePage()">mdi mdi-bell-ring</v-icon>
         </v-badge>
-
       </label>
-      <v-snackbar top="top" v-model="snackNotification" location="right" :timeout="5000" :color="snackNotificationColor">
+      <v-snackbar
+        top="top"
+        v-model="snackNotification"
+        location="right"
+        :timeout="5000"
+        :color="snackNotificationColor"
+      >
         {{ snackNotificationText }}
 
         <template v-slot:action="{ attrs }">
@@ -137,12 +201,24 @@
         <nuxt />
       </v-container>
     </v-main>
-    <v-btn height="50" width="20" dark :color="changeColor" class="fixed-setting"
-      @click.stop="rightDrawer = !rightDrawer">
+    <v-btn
+      height="50"
+      width="20"
+      dark
+      :color="changeColor"
+      class="fixed-setting"
+      @click.stop="rightDrawer = !rightDrawer"
+    >
       <v-icon class="spin" dark size="25">mdi-cog</v-icon>
     </v-btn>
     <!-- setting -->
-    <v-navigation-drawer v-model="rightDrawer" :clipped="true" :right="right" fixed style="z-index: 1000">
+    <v-navigation-drawer
+      v-model="rightDrawer"
+      :clipped="true"
+      :right="right"
+      fixed
+      style="z-index: 1000"
+    >
       <v-row style="margin-top: 50px">
         <v-col>
           <v-card class="pa-2" elevation="0">
@@ -150,12 +226,30 @@
               <div class="mb-3">
                 <Strong>Theme</Strong>
               </div>
-              <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
-                <input type="radio" class="btn-check" name="theme" id="light" autocomplete="off"
-                  @click="changeTheme('light')" />
-                <label class="btn" :class="'btn-outline-dark'" for="light">Light</label>
-                <input type="radio" class="btn-check" name="theme" id="dark" autocomplete="off"
-                  @click="changeTheme('dark')" />
+              <div
+                class="btn-group"
+                role="group"
+                aria-label="Basic radio toggle button group"
+              >
+                <input
+                  type="radio"
+                  class="btn-check"
+                  name="theme"
+                  id="light"
+                  autocomplete="off"
+                  @click="changeTheme('light')"
+                />
+                <label class="btn" :class="'btn-outline-dark'" for="light"
+                  >Light</label
+                >
+                <input
+                  type="radio"
+                  class="btn-check"
+                  name="theme"
+                  id="dark"
+                  autocomplete="off"
+                  @click="changeTheme('dark')"
+                />
                 <label class="btn btn-outline-dark" for="dark">Dark</label>
               </div>
             </v-col>
@@ -165,14 +259,38 @@
                 <Strong>Top Bar</Strong>
               </div>
               <div class="d-flex">
-                <v-btn class="mx-2 stg-color-icon" fab dark x-small color="primary"
-                  @click="changeTopBarColor('primary')"></v-btn>
-                <v-btn class="mx-2 stg-color-icon" fab dark x-small color="error"
-                  @click="changeTopBarColor('error')"></v-btn>
-                <v-btn class="mx-2 stg-color-icon" fab dark x-small color="indigo"
-                  @click="changeTopBarColor('indigo')"></v-btn>
-                <v-btn class="mx-2 stg-color-icon" fab dark x-small color="background"
-                  @click="changeTopBarColor('background')"></v-btn>
+                <v-btn
+                  class="mx-2 stg-color-icon"
+                  fab
+                  dark
+                  x-small
+                  color="primary"
+                  @click="changeTopBarColor('primary')"
+                ></v-btn>
+                <v-btn
+                  class="mx-2 stg-color-icon"
+                  fab
+                  dark
+                  x-small
+                  color="error"
+                  @click="changeTopBarColor('error')"
+                ></v-btn>
+                <v-btn
+                  class="mx-2 stg-color-icon"
+                  fab
+                  dark
+                  x-small
+                  color="indigo"
+                  @click="changeTopBarColor('indigo')"
+                ></v-btn>
+                <v-btn
+                  class="mx-2 stg-color-icon"
+                  fab
+                  dark
+                  x-small
+                  color="background"
+                  @click="changeTopBarColor('background')"
+                ></v-btn>
               </div>
             </v-col>
             <v-divider></v-divider>
@@ -181,14 +299,38 @@
                 <Strong>Side Bar</Strong>
               </div>
               <div class="d-flex">
-                <v-btn class="mx-2 stg-color-icon" fab dark x-small color="primary"
-                  @click="changeSideBarColor('primary')"></v-btn>
-                <v-btn class="mx-2 stg-color-icon" fab dark x-small color="error"
-                  @click="changeSideBarColor('error')"></v-btn>
-                <v-btn class="mx-2 stg-color-icon" fab dark x-small color="indigo"
-                  @click="changeSideBarColor('indigo')"></v-btn>
-                <v-btn class="mx-2 stg-color-icon" fab dark x-small color="background"
-                  @click="changeSideBarColor('background')">
+                <v-btn
+                  class="mx-2 stg-color-icon"
+                  fab
+                  dark
+                  x-small
+                  color="primary"
+                  @click="changeSideBarColor('primary')"
+                ></v-btn>
+                <v-btn
+                  class="mx-2 stg-color-icon"
+                  fab
+                  dark
+                  x-small
+                  color="error"
+                  @click="changeSideBarColor('error')"
+                ></v-btn>
+                <v-btn
+                  class="mx-2 stg-color-icon"
+                  fab
+                  dark
+                  x-small
+                  color="indigo"
+                  @click="changeSideBarColor('indigo')"
+                ></v-btn>
+                <v-btn
+                  class="mx-2 stg-color-icon"
+                  fab
+                  dark
+                  x-small
+                  color="background"
+                  @click="changeSideBarColor('background')"
+                >
                 </v-btn>
               </div>
             </v-col>
@@ -478,7 +620,6 @@ export default {
     };
   },
   created() {
-
     let das = {
       icon: "mdi-home",
       title: "Dashboard",
@@ -487,9 +628,6 @@ export default {
     };
     let user = this.$auth.user;
     let permissions = user.permissions;
-
-
-
 
     if (user && user.is_master) {
       this.items = this.menus;
@@ -504,15 +642,14 @@ export default {
     });
 
     this.getCompanyDetails();
-
   },
 
   mounted() {
     this.verifyLeaveNotificationsApi();
 
     setInterval(() => {
-
-      if (this.socketConnectionStatus != 1) { //socket connection is closed
+      if (this.socketConnectionStatus != 1) {
+        //socket connection is closed
         this.verifyLeaveNotificationsSocket();
       }
     }, 1000 * 60);
@@ -521,9 +658,6 @@ export default {
     // socketConnectionStatus(val) {
     //   console.log('watch ', val);
     // },
-
-
-
   },
   computed: {
     changeColor() {
@@ -553,9 +687,6 @@ export default {
       this.$router.push("/leaves");
     },
     verifyLeaveNotificationsSocket() {
-
-
-
       // 0	CONNECTING	Socket has been created.The connection is not yet open.
       // 1	OPEN	The connection is open and ready to communicate.
       // 2	CLOSING	The connection is in the process of closing.
@@ -563,74 +694,61 @@ export default {
 
       let company_id = this.$auth.user.company.id;
 
-
       if (!process.env.ADMIN_LEAVE_NOTIFICATION_SOCKET_ENDPOINT) return false;
-      this.socket = new WebSocket(process.env.ADMIN_LEAVE_NOTIFICATION_SOCKET_ENDPOINT);
+      this.socket = new WebSocket(
+        process.env.ADMIN_LEAVE_NOTIFICATION_SOCKET_ENDPOINT
+      );
 
       this.socket.onopen = function () {
-
         this.socketConnectionStatus = this.socket.readyState;
 
         const data = {
           company_id: company_id,
-
         };
         this.socket.send(JSON.stringify(data)); // this works
-
       };
       this.socket.onclose = function () {
-
         this.socketConnectionStatus = 0;
-
       };
       this.socket.onmessage = ({ data }) => {
-
-
-
         data = JSON.parse(data);
         if (data.status && data.new_leaves_data[0]) {
-
           let element = data.new_leaves_data[0];
           //data.new_leaves_data.data.forEach(element => {
 
           this.snackNotification = true;
-          this.snackNotificationText = "New Leave Notification - From : " + element.first_name + " " + element.last_name;
-
-
+          this.snackNotificationText =
+            "New Leave Notification - From : " +
+            element.first_name +
+            " " +
+            element.last_name;
         }
         this.pendingLeavesCount = data.total_pending_count;
       };
-
-
     },
     verifyLeaveNotificationsApi() {
-
       if (!this.$auth.user?.company?.id) return false;
       let options = {
         params: {
-          company_id: this.$auth.user?.company?.id || 0
-        }
+          company_id: this.$auth.user?.company?.id || 0,
+        },
       };
 
+      this.$axios.get(`employee_leaves_new`, options).then(({ data }) => {
+        if (data.status) {
+          data.new_leaves_data.data.forEach((element) => {
+            this.snackNotification = true;
 
-      this.$axios
-        .get(`employee_leaves_new`, options)
-        .then(({ data }) => {
-          if (data.status) {
+            this.snackNotificationText =
+              "New Leave Notification : From : " +
+              element.employee.first_name +
+              " " +
+              element.employee.last_name;
+          });
 
-
-            data.new_leaves_data.data.forEach(element => {
-              this.snackNotification = true;
-
-              this.snackNotificationText = "New Leave Notification : From : " + element.employee.first_name + " " + element.employee.last_name;;
-            });
-
-
-            this.pendingLeavesCount = data.total_pending_count;
-          }
-
-
-        });
+          this.pendingLeavesCount = data.total_pending_count;
+        }
+      });
     },
     collapseSubItems() {
       this.menus.map((item) => (item.active = false));
@@ -699,7 +817,7 @@ export default {
   },
 };
 </script>
-<style>
+<!-- <style>
 table {
   font-family: Roboto !important;
 }
@@ -1041,4 +1159,4 @@ tbody tr:nth-of-type(odd) {
     display: block;
   }
 }
-</style>
+</style> -->
