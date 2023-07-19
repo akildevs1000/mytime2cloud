0ad629984d9a302c0fa0076788a660d7d70d67a3
akildevs1002 <akildevs1002@gmail.com>
2023-07-19 19:32:15 +0400

    CSS

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,65 +1,35 @@
 <template>
   <v-app>
-    <v-navigation-drawer
-      v-model="drawer"
-      dark
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-      :color="sideBarcolor"
-      :style="miniVariant ? 'width: 60px' : ''"
-      @transitionend="collapseSubItems"
-    >
+    <v-navigation-drawer v-model="drawer" dark :mini-variant="miniVariant" :clipped="clipped" fixed app
+      :color="sideBarcolor" :style="miniVariant ? 'width: 60px' : ''" @transitionend="collapseSubItems">
       <br />
       <v-list v-for="(i, idx) in items" :key="idx" style="padding: 5px 0 0 0px">
-        <v-list-item
-          :to="i.to"
-          router
-          v-if="!i.hasChildren"
-          :class="!miniVariant || 'pl-2'"
-        >
+        <v-list-item :to="i.to" router v-if="!i.hasChildren" :class="!miniVariant || 'pl-2'">
           <v-list-item-icon class="ma-2">
             <v-icon>{{ i.icon }}</v-icon>
           </v-list-item-icon>
           <v-list-item-title> {{ i.title }}&nbsp; </v-list-item-title>
         </v-list-item>
-        <v-list-item
-          v-else
-          :class="!miniVariant || 'pl-2'"
-          @click="i.open_menu = !i.open_menu"
-        >
+        <v-list-item v-else :class="!miniVariant || 'pl-2'" @click="i.open_menu = !i.open_menu">
           <v-list-item-icon class="ma-2">
             <v-icon>{{ i.icon }}</v-icon>
-            <v-icon v-if="miniVariant" small
-              >{{ !i.open_menu ? "mdi-chevron-down" : "mdi-chevron-up" }}
+            <v-icon v-if="miniVariant" small>{{ !i.open_menu ? "mdi-chevron-down" : "mdi-chevron-up" }}
             </v-icon>
           </v-list-item-icon>
 
           <v-list-item-title>{{ i.title }} </v-list-item-title>
-          <v-icon small
-            >{{ !i.open_menu ? "mdi-chevron-down" : "mdi-chevron-up" }}
+          <v-icon small>{{ !i.open_menu ? "mdi-chevron-down" : "mdi-chevron-up" }}
           </v-icon>
         </v-list-item>
         <div v-if="i.open_menu">
-          <div
-            style="margin-left: 54px"
-            v-for="(j, jdx) in i.hasChildren"
-            :key="jdx"
-          >
+          <div style="margin-left: 54px" v-for="(j, jdx) in i.hasChildren" :key="jdx">
             <!-- v-show="!miniVariant" -->
             <v-list-item style="min-height: 0" :to="j.to" class="submenutitle">
-              <v-list-item-title v-if="!miniVariant"
-                >{{ j.title }}
+              <v-list-item-title v-if="!miniVariant">{{ j.title }}
               </v-list-item-title>
 
-              <v-list-item-icon
-                :style="miniVariant ? 'margin-left: -54px;' : ''"
-              >
-                <v-icon
-                  :to="j.to"
-                  :style="miniVariant ? 'margin-left: 12px;' : ''"
-                >
+              <v-list-item-icon :style="miniVariant ? 'margin-left: -54px;' : ''">
+                <v-icon :to="j.to" :style="miniVariant ? 'margin-left: 12px;' : ''">
                   {{ j.icon }}
                 </v-icon>
               </v-list-item-icon>
@@ -73,14 +43,8 @@
     margin-left: -49px;
 " -->
 
-    <v-app-bar
-      :color="changeColor"
-      dark
-      :clipped-left="clipped"
-      fixed
-      app
-      :style="$nuxt.$route.name == 'index' ? 'z-index: 100000' : ''"
-    >
+    <v-app-bar :color="changeColor" dark :clipped-left="clipped" fixed app
+      :style="$nuxt.$route.name == 'index' ? 'z-index: 100000' : ''">
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
       <v-btn icon @click.stop="miniVariant = !miniVariant">
         <v-icon>mdi-{{ `chevron-${miniVariant ? "right" : "left"}` }}</v-icon>
@@ -91,15 +55,8 @@
       <span class="text-overflow">{{ title }}</span>
       <v-spacer></v-spacer>
 
-      <v-menu
-        nudge-bottom="50"
-        transition="scale-transition"
-        origin="center center"
-        bottom
-        left
-        min-width="200"
-        nudge-left="20"
-      >
+      <v-menu nudge-bottom="50" transition="scale-transition" origin="center center" bottom left min-width="200"
+        nudge-left="20">
         <template v-slot:activator="{ on, attrs }">
           <label class="px-2 text-overflow" v-bind="attrs" v-on="on">
             <!-- {{ getUser }} -->
@@ -107,10 +64,7 @@
 
           <v-btn icon color="yellow" v-bind="attrs" v-on="on">
             <v-avatar>
-              <v-img
-                style="width: 50px"
-                :src="`https://ui-avatars.com/api/?name=${getUser}`"
-              ></v-img>
+              <v-img style="width: 50px" :src="`https://ui-avatars.com/api/?name=${getUser}`"></v-img>
               <!-- <img :src="getLogo || '/no-image.PNG'" /> -->
             </v-avatar>
           </v-btn>
@@ -123,9 +77,7 @@
                 <v-icon>mdi-account-multiple-outline</v-icon>
               </v-list-item-icon>
               <v-list-item-content>
-                <v-list-item-title class="black--text"
-                  >Profile</v-list-item-title
-                >
+                <v-list-item-title class="black--text">Profile</v-list-item-title>
               </v-list-item-content>
             </v-list-item>
 
@@ -134,9 +86,7 @@
                 <v-icon>mdi-cog</v-icon>
               </v-list-item-icon>
               <v-list-item-content>
-                <v-list-item-title class="black--text"
-                  >Setting</v-list-item-title
-                >
+                <v-list-item-title class="black--text">Setting</v-list-item-title>
               </v-list-item-content>
             </v-list-item>
 
@@ -145,9 +95,7 @@
                 <v-icon>mdi-logout</v-icon>
               </v-list-item-icon>
               <v-list-item-content>
-                <v-list-item-title class="black--text"
-                  >Logout</v-list-item-title
-                >
+                <v-list-item-title class="black--text">Logout</v-list-item-title>
               </v-list-item-content>
             </v-list-item>
           </v-list-item-group>
@@ -162,30 +110,15 @@
             pendingLeavesCount }}</strong></v-chip>
         </span> -->
 
-        <v-badge
-          v-if="pendingLeavesCount > 0"
-          @click="navigateToLeavePage()"
-          :color="pendingLeavesCount > 0 ? 'red' : 'black'"
-          :content="pendingLeavesCount"
-        >
+        <v-badge v-if="pendingLeavesCount > 0" @click="navigateToLeavePage()"
+          :color="pendingLeavesCount > 0 ? 'red' : 'black'" :content="pendingLeavesCount">
           <v-icon @click="navigateToLeavePage()">mdi mdi-bell-ring</v-icon>
         </v-badge>
-        <v-badge
-          v-else
-          @click="navigateToLeavePage()"
-          color="black"
-          content="0"
-        >
+        <v-badge v-else @click="navigateToLeavePage()" color="black" content="0">
           <v-icon @click="navigateToLeavePage()">mdi mdi-bell-ring</v-icon>
         </v-badge>
       </label>
-      <v-snackbar
-        top="top"
-        v-model="snackNotification"
-        location="right"
-        :timeout="5000"
-        :color="snackNotificationColor"
-      >
+      <v-snackbar top="top" v-model="snackNotification" location="right" :timeout="5000" :color="snackNotificationColor">
         {{ snackNotificationText }}
 
         <template v-slot:action="{ attrs }">
@@ -201,24 +134,12 @@
         <nuxt />
       </v-container>
     </v-main>
-    <v-btn
-      height="50"
-      width="20"
-      dark
-      :color="changeColor"
-      class="fixed-setting"
-      @click.stop="rightDrawer = !rightDrawer"
-    >
+    <v-btn height="50" width="20" dark :color="changeColor" class="fixed-setting"
+      @click.stop="rightDrawer = !rightDrawer">
       <v-icon class="spin" dark size="25">mdi-cog</v-icon>
     </v-btn>
     <!-- setting -->
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :clipped="true"
-      :right="right"
-      fixed
-      style="z-index: 1000"
-    >
+    <v-navigation-drawer v-model="rightDrawer" :clipped="true" :right="right" fixed style="z-index: 1000">
       <v-row style="margin-top: 50px">
         <v-col>
           <v-card class="pa-2" elevation="0">
@@ -226,30 +147,12 @@
               <div class="mb-3">
                 <Strong>Theme</Strong>
               </div>
-              <div
-                class="btn-group"
-                role="group"
-                aria-label="Basic radio toggle button group"
-              >
-                <input
-                  type="radio"
-                  class="btn-check"
-                  name="theme"
-                  id="light"
-                  autocomplete="off"
-                  @click="changeTheme('light')"
-                />
-                <label class="btn" :class="'btn-outline-dark'" for="light"
-                  >Light</label
-                >
-                <input
-                  type="radio"
-                  class="btn-check"
-                  name="theme"
-                  id="dark"
-                  autocomplete="off"
-                  @click="changeTheme('dark')"
-                />
+              <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
+                <input type="radio" class="btn-check" name="theme" id="light" autocomplete="off"
+                  @click="changeTheme('light')" />
+                <label class="btn" :class="'btn-outline-dark'" for="light">Light</label>
+                <input type="radio" class="btn-check" name="theme" id="dark" autocomplete="off"
+                  @click="changeTheme('dark')" />
                 <label class="btn btn-outline-dark" for="dark">Dark</label>
               </div>
             </v-col>
@@ -259,38 +162,14 @@
                 <Strong>Top Bar</Strong>
               </div>
               <div class="d-flex">
-                <v-btn
-                  class="mx-2 stg-color-icon"
-                  fab
-                  dark
-                  x-small
-                  color="primary"
-                  @click="changeTopBarColor('primary')"
-                ></v-btn>
-                <v-btn
-                  class="mx-2 stg-color-icon"
-                  fab
-                  dark
-                  x-small
-                  color="error"
-                  @click="changeTopBarColor('error')"
-                ></v-btn>
-                <v-btn
-                  class="mx-2 stg-color-icon"
-                  fab
-                  dark
-                  x-small
-                  color="indigo"
-                  @click="changeTopBarColor('indigo')"
-                ></v-btn>
-                <v-btn
-                  class="mx-2 stg-color-icon"
-                  fab
-                  dark
-                  x-small
-                  color="background"
-                  @click="changeTopBarColor('background')"
-                ></v-btn>
+                <v-btn class="mx-2 stg-color-icon" fab dark x-small color="primary"
+                  @click="changeTopBarColor('primary')"></v-btn>
+                <v-btn class="mx-2 stg-color-icon" fab dark x-small color="error"
+                  @click="changeTopBarColor('error')"></v-btn>
+                <v-btn class="mx-2 stg-color-icon" fab dark x-small color="indigo"
+                  @click="changeTopBarColor('indigo')"></v-btn>
+                <v-btn class="mx-2 stg-color-icon" fab dark x-small color="background"
+                  @click="changeTopBarColor('background')"></v-btn>
               </div>
             </v-col>
             <v-divider></v-divider>
@@ -299,38 +178,14 @@
                 <Strong>Side Bar</Strong>
               </div>
               <div class="d-flex">
-                <v-btn
-                  class="mx-2 stg-color-icon"
-                  fab
-                  dark
-                  x-small
-                  color="primary"
-                  @click="changeSideBarColor('primary')"
-                ></v-btn>
-                <v-btn
-                  class="mx-2 stg-color-icon"
-                  fab
-                  dark
-                  x-small
-                  color="error"
-                  @click="changeSideBarColor('error')"
-                ></v-btn>
-                <v-btn
-                  class="mx-2 stg-color-icon"
-                  fab
-                  dark
-                  x-small
-                  color="indigo"
-                  @click="changeSideBarColor('indigo')"
-                ></v-btn>
-                <v-btn
-                  class="mx-2 stg-color-icon"
-                  fab
-                  dark
-                  x-small
-                  color="background"
-                  @click="changeSideBarColor('background')"
-                >
+                <v-btn class="mx-2 stg-color-icon" fab dark x-small color="primary"
+                  @click="changeSideBarColor('primary')"></v-btn>
+                <v-btn class="mx-2 stg-color-icon" fab dark x-small color="error"
+                  @click="changeSideBarColor('error')"></v-btn>
+                <v-btn class="mx-2 stg-color-icon" fab dark x-small color="indigo"
+                  @click="changeSideBarColor('indigo')"></v-btn>
+                <v-btn class="mx-2 stg-color-icon" fab dark x-small color="background"
+                  @click="changeSideBarColor('background')">
                 </v-btn>
               </div>
             </v-col>
@@ -817,7 +672,7 @@ export default {
   },
 };
 </script>
-<!-- <style>
+<style>
 table {
   font-family: Roboto !important;
 }
@@ -1159,4 +1014,4 @@ tbody tr:nth-of-type(odd) {
     display: block;
   }
 }
-</style> -->
+</style>
