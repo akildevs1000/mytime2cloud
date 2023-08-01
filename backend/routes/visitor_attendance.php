8ca480c274da41631263a852a70f3e5d4ad7b0aa
akildevs1000 <107389124+akildevs1000@users.noreply.github.com>
2023-08-01 19:29:27 +0400

    added weekly

--- backend/routes/visitor_attendance.php
+++ backend/routes/visitor_attendance.php
@@ -7,5 +7,6 @@ Route::apiResource('visitor_attendance', VisitorAttendanceController::class);
 Route::get('visitor_monthly', [VisitorAttendanceController::class, "monthly_pdf"]);
 Route::get('visitor_weekly', [VisitorAttendanceController::class, "monthly_pdf"]);
 Route::get('visitor_monthly_download_pdf', [VisitorAttendanceController::class, "monthly_download_pdf"]);
+Route::get('visitor_weekly_download_pdf', [VisitorAttendanceController::class, "monthly_download_pdf"]);
 Route::get('visitor_weekly_download_csv', [VisitorAttendanceController::class, "monthly_download_csv"]);
 Route::get('visitor_monthly_download_csv', [VisitorAttendanceController::class, "monthly_download_csv"]);
