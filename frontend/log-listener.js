8e7ab895311674e89de853c9570ffdf7020b0250
akildevs1000 <107389124+akildevs1000@users.noreply.github.com>
2023-08-14 13:09:57 +0400

    added log listener seperate file

---
+++ frontend/log-listener.js
@@ -0,0 +1,38 @@
+const WebSocket = require("ws");
+const fs = require("fs");
+require("dotenv").config();
+
+const { SOCKET_ENDPOINT } = `wss://sdk.ideahrms.com/WebSocket`; //process.env;
+
+// Create a WebSocket connection
+const socket = new WebSocket(SOCKET_ENDPOINT);
+
+// Handle WebSocket connection events
+socket.onopen = () => {
+    console.log(`Connected to ${SOCKET_ENDPOINT}`);
+};
+
+socket.onerror = (error) => {
+    console.error("WebSocket error:", error.message);
+};
+
+// Create and append headers to the CSV log file
+const logFilePath = "../backend/storage/logs.csv";
+// const header = "UserID,LogTime,DeviceID,SerialNumber\n";
+// fs.writeFileSync(logFilePath, header, { flag: "w" }); // Use "w" flag to overwrite/create the file
+
+// Handle incoming WebSocket messages
+socket.onmessage = ({ data }) => {
+    try {
+        const jsonData = JSON.parse(data).Data;
+        const { UserCode, SN, RecordDate, RecordNumber } = jsonData;
+
+        if (UserCode > 0) {
+            const logEntry = `${UserCode},${SN},${RecordDate},${RecordNumber}`;
+            fs.appendFileSync(logFilePath, logEntry + "\n");
+            console.log(logEntry);
+        }
+    } catch (error) {
+        console.error("Error processing message:", error.message);
+    }
+};
