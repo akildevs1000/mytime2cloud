3eb53330fc84e48bf0b92f1b721ebbb651cf769e
akildevs1000 <107389124+akildevs1000@users.noreply.github.com>
2023-08-15 19:15:01 +0400

    added

--- frontend/pages/date_range_picker_test.vue
+++ frontend/pages/date_range_picker_test.vue
@@ -1,81 +1,120 @@
 <template>
-  <v-row>
-    <v-col cols="12" sm="6" md="4">
-      <v-menu
-        ref="menu"
-        v-model="menu"
-        :close-on-content-click="false"
-        transition="scale-transition"
-        offset-y
-        min-width="auto"
+  <v-menu
+    ref="menu"
+    v-model="menu"
+    :close-on-content-click="false"
+    transition="scale-transition"
+    offset-y
+    min-width="auto"
+  >
+    <template v-slot:activator="{ on }">
+      <v-text-field
+        :hide-details="true"
+        outlined
+        dense
+        v-on="on"
+        v-model="finalDates"
+        readonly
+        placeholder="Select Date Range"
+      ></v-text-field>
+    </template>
+
+    <div class="text-center" style="padding: 0">
+      <v-date-picker
+        @input="firstPicker"
+        persistant
+        no-title
+        color="primary"
+        v-model="startDates"
+        range
+        scrollable
       >
-        <template v-slot:activator="{ on, attrs }">
-          <v-text-field
-            v-model="startDate"
-            label="Picker in menu"
-            prepend-icon="mdi-calendar"
-            readonly
-            v-bind="attrs"
-            v-on="on"
-          ></v-text-field>
-        </template>
-        <v-date-picker v-model="startDate" no-title range>
-          <v-spacer></v-spacer>
+      </v-date-picker>
 
-          <v-btn small dark color="background" @click="menu = false">
-            Cancel
-          </v-btn>
-          <v-btn
-            small
-            dark
-            color="background"
-            @click="$refs.menu.save(dateRangeText)"
-          >
-            OK
-          </v-btn>
-        </v-date-picker>
+      <v-date-picker
+        @input="secondPicker"
+        no-title
+        color="primary"
+        v-model="endDates"
+        range
+        :min="min"
+      >
+      </v-date-picker>
+      <v-card class="text-right pa-2">
+        <v-btn dark color="background" x-small @click="clearDates">
+          clear
+        </v-btn>
 
-        <v-date-picker
-          @input="handleDateChange"
-          ref="datePicker"
-          v-model="startDate"
-          no-title
-          range
+        <v-btn dark color="background" x-small @click="menu = false">
+          close
+        </v-btn>
+
+        <v-btn
+          dark
+          color="background"
+          x-small
+          @click="$refs.menu.save(secondPicker)"
         >
-          <v-spacer></v-spacer>
-
-          <v-btn small dark color="background" @click="menu = false">
-            Cancel
-          </v-btn>
-          <v-btn
-            small
-            dark
-            color="background"
-            @click="$refs.menu.save(dateRangeText)"
-          >
-            OK
-          </v-btn>
-        </v-date-picker>
-      </v-menu>
-    </v-col>
-  </v-row>
+          OK
+        </v-btn>
+      </v-card>
+    </div>
+  </v-menu>
 </template>
-<script>
-const currentDate = new Date();
-const nextMonthDate = new Date(currentDate);
-nextMonthDate.setMonth(nextMonthDate.getMonth() + 1);
 
+<script>
+let currentFullDate = new Date(
+  Date.now() - new Date().getTimezoneOffset() * 60000
+);
+
+let nextFullDate = new Date(currentFullDate);
+nextFullDate.setMonth(nextFullDate.getMonth() + 1);
+let currentDate = currentFullDate.toISOString().slice(0, 10);
+let nextDate = nextFullDate.toISOString().slice(0, 10);
 export default {
   data: () => ({
-    startTime: "",
-    endTime: "",
-    initialValue: "2023-09-30",
-    showStartTimePicker: false,
-    showEndTimePicker: false,
-
-    startDate: [],
-    endDate: [],
     menu: false,
+    menu2: false,
+
+    showPicker: true,
+
+    startDates: [currentDate],
+    endDates: [nextDate],
+    finalDates: [],
+    min: nextDate,
   }),
+  methods: {
+    secondPicker() {
+      this.endDates.sort((a, b) => a.localeCompare(b));
+
+      let endDate = this.endDates.length
+        ? this.endDates[this.endDates.length - 1]
+        : this.startDates[this.startDates.length - 1];
+
+      this.finalDates = [this.startDates[0], endDate];
+
+      this.startDates = this.finalDates;
+      this.endDates = this.finalDates;
+
+      this.min = this.startDates[0];
+    },
+
+    firstPicker() {
+      this.startDates.sort((a, b) => a.localeCompare(b));
+      this.endDates = [];
+    },
+    clearDates() {
+      this.clearStartDates();
+      this.clearEndDates();
+    },
+    clearStartDates() {
+      this.finalDates = [];
+      this.startDates = [];
+    },
+    clearEndDates() {
+      this.finalDates = [];
+      this.endDates = [];
+    },
+  },
 };
 </script>
