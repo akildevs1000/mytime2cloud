d927c4ee5296ec183f99f80f81a93a64ac51b75f
akildevs1000 <107389124+akildevs1000@users.noreply.github.com>
2023-08-19 19:08:31 +0400

    addd

--- frontend/pages/camera.vue
+++ frontend/pages/camera.vue
@@ -2,26 +2,14 @@
   <div>
     <h1>Camera Example</h1>
     <v-container>
-      <v-btn @click="open_camera = !open_camera">open_camera</v-btn>
+      <v-btn @click="capturePhoto">Capture</v-btn>
 
       <v-row>
         <v-col cols="6">
-          <video
-            v-if="open_camera"
-            style="border: 1px solid; width: 100%; height: 175px"
-            id="camera"
-            autoplay
-            playsinline
-            ref="camera"
-          ></video>
-          <v-btn v-if="open_camera" @click="capturePhoto">Capture</v-btn>
-
+          <video style="width: 300px; height: 300px;" id="camera" autoplay playsinline ref="camera"></video>
         </v-col>
         <v-col cols="6">
-          <img
-            style="width: 300px; height: 300px"
-            :src="previewImage"
-          />
+          <img style="width: 300px; height: 300px;" v-if="capturedImage" :src="capturedImage" />
         </v-col>
       </v-row>
     </v-container>
@@ -32,38 +20,27 @@
 export default {
   data() {
     return {
-      open_camera: false,
       errorMessage: null,
-      previewImage: null,
-      facingMode: "environment",
-      cameraStream: null, // Store the camera stream
+      capturedImage: null,
     };
   },
   methods: {
     async startCamera() {
       try {
-        this.cameraStream = await navigator.mediaDevices.getUserMedia({
-          video: { facingMode: "user" },
+        const stream = await navigator.mediaDevices.getUserMedia({
+          video: { facingMode: 'user' }, // Requesting front camera
         });
-        this.$refs.camera.srcObject = this.cameraStream;
+        this.$refs.camera.srcObject = stream;
         this.errorMessage = null;
       } catch (error) {
-        console.error("Error accessing camera:", error);
-        this.errorMessage = "Camera not found or access denied.";
+        console.error('Error accessing camera:', error);
+        this.errorMessage = 'Camera not found or access denied.';
       }
-    },
-    async toggleCamera() {
-      if (this.cameraStream) {
-        this.cameraStream.getTracks().forEach((track) => track.stop()); // Stop the current camera stream
-      }
-      this.facingMode =
-        this.facingMode === "environment" ? "user" : "environment";
-      await this.startCamera(); // Start the camera with the new facing mode
     },
     capturePhoto() {
       const cameraElement = this.$refs.camera;
-      const canvasElement = document.createElement("canvas");
-      const canvasContext = canvasElement.getContext("2d");
+      const canvasElement = document.createElement('canvas');
+      const canvasContext = canvasElement.getContext('2d');
 
       canvasElement.width = cameraElement.videoWidth;
       canvasElement.height = cameraElement.videoHeight;
@@ -75,16 +52,11 @@ export default {
         canvasElement.height
       );
 
-      this.previewImage = canvasElement.toDataURL("image/png");
+      this.capturedImage = canvasElement.toDataURL('image/png');
     },
   },
   mounted() {
     this.startCamera();
-  },
-  beforeDestroy() {
-    if (this.cameraStream) {
-      this.cameraStream.getTracks().forEach((track) => track.stop()); // Make sure to stop the camera stream when the component is destroyed
-    }
   },
 };
 </script>
