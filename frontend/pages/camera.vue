72766498047f3f1c18ae2dbf2957e8848b15dd83
akildevs1000 <107389124+akildevs1000@users.noreply.github.com>
2023-08-19 19:52:30 +0400

    added

--- frontend/pages/camera.vue
+++ frontend/pages/camera.vue
@@ -6,12 +6,20 @@
 
       <v-row>
         <v-col cols="6">
-
-          <video style="width: 300px;height: 300px;" id="camera" autoplay playsinline ref="camera"></video>
+          <video
+            style="border: 1px solid; width: 100%; height: 300px"
+            id="camera"
+            autoplay
+            playsinline
+            ref="camera"
+          ></video>
+          <img
+            style="width: 300px; height: 300px"
+            v-if="capturedImage"
+            :src="capturedImage"
+          />
         </v-col>
-        <v-col cols="6">
-          <img style="width: 300px;height: 300px;" v-if="capturedImage" :src="capturedImage" />
-        </v-col>
+        <v-col cols="6"> </v-col>
       </v-row>
     </v-container>
   </div>
@@ -29,19 +37,19 @@ export default {
     async startCamera() {
       try {
         const stream = await navigator.mediaDevices.getUserMedia({
-          video: { facingMode: 'user' }, // Example: using rear camera
+          video: { facingMode: "environment" }, // Example: using rear camera
         });
         this.$refs.camera.srcObject = stream;
         this.errorMessage = null;
       } catch (error) {
-        console.error('Error accessing camera:', error);
-        this.errorMessage = 'Camera not found or access denied.';
+        console.error("Error accessing camera:", error);
+        this.errorMessage = "Camera not found or access denied.";
       }
     },
     capturePhoto() {
       const cameraElement = this.$refs.camera;
-      const canvasElement = document.createElement('canvas');
-      const canvasContext = canvasElement.getContext('2d');
+      const canvasElement = document.createElement("canvas");
+      const canvasContext = canvasElement.getContext("2d");
 
       canvasElement.width = cameraElement.videoWidth;
       canvasElement.height = cameraElement.videoHeight;
@@ -53,7 +61,7 @@ export default {
         canvasElement.height
       );
 
-      this.capturedImage = canvasElement.toDataURL('image/png');
+      this.capturedImage = canvasElement.toDataURL("image/png");
     },
   },
   mounted() {
