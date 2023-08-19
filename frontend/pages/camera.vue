f3684869689f1d3aaaab6dfde0290e2c567a19ac
akildevs1000 <107389124+akildevs1000@users.noreply.github.com>
2023-08-19 13:16:59 +0400

    added

--- frontend/pages/camera.vue
+++ frontend/pages/camera.vue
@@ -1,15 +1,63 @@
 <template>
   <div>
-    <CameraComponent />
+    <h1>Camera Example</h1>
+    <v-alert v-if="errorMessage" class="red" dark>{{ errorMessage }}</v-alert>
+    <v-container v-if="!errorMessage && !loading">
+      <video id="camera" autoplay playsinline ref="camera"></video>
+      <v-button @click="capturePhoto">Capture</v-button>
+      <canvas id="canvas" style="display: none"></canvas>
+    </v-container>
   </div>
 </template>
 
 <script>
-import CameraComponent from "../components/Snippets/Camera";
-
 export default {
-  components: {
-    CameraComponent,
+  data() {
+    return {
+      loading: true,
+      errorMessage: null,
+    };
+  },
+  methods: {
+    async startCamera() {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({
+          video: { facingMode: "environment" }, // Example: using rear camera
+        });
+        this.$refs.camera.srcObject = stream;
+        this.errorMessage = null;
+      } catch (error) {
+        console.error("Error accessing camera:", error);
+        this.errorMessage = "Camera not found or access denied.";
+      }
+    },
+    capturePhoto() {
+      const cameraElement = this.$refs.camera;
+      const canvasElement = document.getElementById("canvas");
+      const canvasContext = canvasElement.getContext("2d");
+
+      canvasElement.width = cameraElement.videoWidth;
+      canvasElement.height = cameraElement.videoHeight;
+      canvasContext.drawImage(
+        cameraElement,
+        0,
+        0,
+        canvasElement.width,
+        canvasElement.height
+      );
+
+      const imageURL = canvasElement.toDataURL("image/png");
+      const preview = document.createElement("img");
+      preview.src = imageURL;
+      document.body.appendChild(preview);
+    },
+  },
+  mounted() {
+    this.startCamera();
   },
 };
 </script>
+
+<style>
+/* Add your custom styles here */
+</style>
