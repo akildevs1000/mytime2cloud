7f1a7245035c023d633a92c09f674c7203da777a
akildevs1000 <107389124+akildevs1000@users.noreply.github.com>
2023-08-19 19:33:49 +0400

    added

--- frontend/pages/employees/index1.vue
+++ frontend/pages/employees/index1.vue
@@ -257,6 +257,8 @@
                     >{{ !upload.name ? "Upload" : "Change" }} Profile Image
                     <v-icon right dark>mdi-cloud-upload</v-icon>
                   </v-btn>
+                  <br />
+
                   <input
                     required
                     type="file"
@@ -264,6 +266,54 @@
                     style="display: none"
                     accept="image/*"
                     ref="attachment_input"
+                  />
+
+                  <!-- <v-btn
+                    style="width: 100%"
+                    small
+                    dark
+                    class="background my-2"
+                    @click="open_camera = !open_camera"
+                    >Take Photo
+                    <v-icon right dark>mdi-camera-outline</v-icon>
+                  </v-btn>
+
+                  <video
+                    v-if="open_camera"
+                    style="
+                      width: 100%;
+                      height: 200px;
+                      border: 1px solid #5fafa3;
+                      border-radius: 50%;
+                      margin: 0 auto;
+                    "
+                    id="camera"
+                    autoplay
+                    playsinline
+                    ref="camera"
+                  ></video> -->
+
+                  <!-- <v-btn
+                    style="width: 100%"
+                    small
+                    dark
+                    class="background my-2"
+                    v-if="open_camera"
+                    @click="capturePhoto"
+                    >Capture
+                  </v-btn> -->
+                  <v-btn @click="capturePhoto">Capture</v-btn>
+                  <video
+                    style="width: 300px; height: 300px"
+                    id="camera"
+                    autoplay
+                    playsinline
+                    ref="camera"
+                  ></video>
+                  <img
+                    style="width: 300px; height: 300px"
+                    v-if="capturedImage"
+                    :src="capturedImage"
                   />
 
                   <span
@@ -982,6 +1032,11 @@ export default {
         value: "options",
       },
     ],
+    open_camera: false,
+    errorMessage: null,
+    facingMode: "environment",
+    cameraStream: null, // Store the camera stream
+    capturedImage: null,
   }),
 
   async created() {
@@ -1001,6 +1056,8 @@ export default {
     this.getTimezone();
   },
   mounted() {
+    this.startCamera();
+
     //this.getDataFromApi();
     this.tabMenu = [
       {
@@ -1084,6 +1141,35 @@ export default {
     },
   },
   methods: {
+    async startCamera() {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({
+          video: { facingMode: "user" }, // Example: using rear camera
+        });
+        this.$refs.camera.srcObject = stream;
+        this.errorMessage = null;
+      } catch (error) {
+        console.error("Error accessing camera:", error);
+        this.errorMessage = "Camera not found or access denied.";
+      }
+    },
+    capturePhoto() {
+      const cameraElement = this.$refs.camera;
+      const canvasElement = document.createElement("canvas");
+      const canvasContext = canvasElement.getContext("2d");
+
+      canvasElement.width = cameraElement.videoWidth;
+      canvasElement.height = cameraElement.videoHeight;
+      canvasContext.drawImage(
+        cameraElement,
+        0,
+        0,
+        canvasElement.width,
+        canvasElement.height
+      );
+
+      this.capturedImage = canvasElement.toDataURL("image/png");
+    },
     getCurrentShift(item) {
       // Define an array of day names
       const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
