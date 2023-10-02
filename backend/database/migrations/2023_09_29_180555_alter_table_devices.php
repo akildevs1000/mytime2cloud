855d31cbbad8c6c0189bfffe24b2e8717f979edd
akildevs1000 <107389124+akildevs1000@users.noreply.github.com>
2023-10-02 18:43:57 +0400

    added

--- backend/database/migrations/2023_09_29_180555_alter_table_devices.php
+++ backend/database/migrations/2023_09_29_180555_alter_table_devices.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::table('[devices]', function (Blueprint $table) {
+        Schema::table('devices', function (Blueprint $table) {
             $table->dropForeign('devices_device_type_check');
         });
     }
@@ -25,7 +25,7 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::table('[devices]', function (Blueprint $table) {
+        Schema::table('devices', function (Blueprint $table) {
             //
         });
     }
