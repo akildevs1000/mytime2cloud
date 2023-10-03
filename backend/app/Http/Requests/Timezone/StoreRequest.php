18420ba06a7ef943a53254d69be0cc48976db064
akildevs1002 <akildevs1002@gmail.com>
2023-10-03 20:03:05 +0400

    Timezone

--- backend/app/Http/Requests/Timezone/StoreRequest.php
+++ backend/app/Http/Requests/Timezone/StoreRequest.php
@@ -35,7 +35,7 @@ class StoreRequest extends FormRequest
             'timezone_name' => ['required', 'min:4', 'max:20',  $contoller->uniqueRecord("timezones", ["timezone_name" => $this->timezone_name] + $companrArr)],
             // 'timezone_id' => ['required', $contoller->uniqueRecord("timezones", ["timezone_id" => $this->timezone_id] + $companrArr)],
             'interval' => ['required', 'array'],
-            'scheduled_days' => ['nullable', 'array'],
+            'scheduled_days' => 'nullable',
             'company_id' => 'required',
             "intervals_raw_data" => 'nullable',
 
