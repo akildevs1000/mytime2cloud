7ebc0c7c5c879b44b11db4ea52b78535b6872b55
akildevs1000 <107389124+akildevs1000@users.noreply.github.com>
2023-10-12 20:57:15 +0400

    test script

---
+++ frontend/requestScript.js
@@ -0,0 +1,30 @@
+const axios = require('axios');
+
+
+const fs = require("fs");
+
+// Define the URL you want to hit
+const url = 'https://backend.ideahrms.com/api/reset_date_time';
+
+// Function to make the HTTP request
+const makeHttpRequest = () => {
+    axios
+        .get(url)
+        .then(({data}) => {
+            // Handle the response if needed
+            console.log('Request successful:', data);
+
+            fs.appendFileSync("./logs.csv", data + "\n");
+        })
+        .catch((error) => {
+            // Handle any errors
+            console.error('Request error:', error.message);
+        });
+};
+
+// Set an interval to make the request every 2 seconds (2000 milliseconds)
+const interval = 10000;
+setInterval(makeHttpRequest, interval);
+
+// Log a message to indicate that the script is running
+console.log(`Requesting ${url} every ${interval / 1000} seconds...`);
