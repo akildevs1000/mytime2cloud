a5400664ecd9acc26d0c79f2381906ead05d6140
akildevs1000 <107389124+akildevs1000@users.noreply.github.com>
2023-10-18 12:36:22 +0400

    added

--- backend/app/Models/Employee.php
+++ backend/app/Models/Employee.php
@@ -361,10 +361,12 @@ class Employee extends Model
                 $q->whereHas('department', fn (Builder $query) => $query->where('branch_id', '=', $request->department_branch_id));
             })
             ->when($request->filled("branch_id"), function ($q) use ($request) {
-                $q->whereHas('department', fn (Builder $query) => $query->where('branch_id', '=', $request->branch_id));
+                $q->where('branch_id', '=', $request->branch_id);
+                // $q->whereHas('department', fn (Builder $query) => $query->where('branch_id', '=', $request->branch_id));
             })
             ->when($request->filled("filter_branch_id"), function ($q) use ($request) {
-                $q->whereHas('department', fn (Builder $query) => $query->where('branch_id', '=', $request->filter_branch_id));
+                $q->where('branch_id', '=', $request->filter_branch_id);
+                // $q->whereHas('department', fn (Builder $query) => $query->where('branch_id', '=', $request->filter_branch_id));
             })
 
 
