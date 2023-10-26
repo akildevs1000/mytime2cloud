08f1e9df1e80ea7d08584edca0ed51a1c50a1034
akildevs1002 <akildevs1002@gmail.com>
2023-10-26 13:17:22 +0400

    mail

--- backend/resources/views/vendor/mail/html/message.blade.php
+++ backend/resources/views/vendor/mail/html/message.blade.php
@@ -4,7 +4,7 @@
 @component('mail::header', ['url' => config('app.url')])
 <!-- {{ config('app.name') }} -->
 
-<img style="width:150px;max-width:150px;height:auto" src="{{   config('base.url').'/logo.png'  }}" alt="{{ config('app.name') }}">
+<img style="width:150px;max-width:150px;height:auto" src="{{   env('BASE_URL').'/logo.png'  }}" alt="{{ config('app.name') }}">
 
 
 
