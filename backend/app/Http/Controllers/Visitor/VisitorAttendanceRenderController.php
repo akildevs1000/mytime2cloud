49c201a277f568e5288034b940bd0cf03d2468a5
Venu J <akildevs1004@example.com>
2023-11-29 20:13:53 +0400

    Visitor

--- backend/app/Http/Controllers/Visitor/VisitorAttendanceRenderController.php
+++ backend/app/Http/Controllers/Visitor/VisitorAttendanceRenderController.php
@@ -108,6 +108,8 @@ class VisitorAttendanceRenderController extends Controller
                 // "shift_id" => $firstLog["schedule"]["shift_id"] ?? 0,
                 // "shift_type_id" => $firstLog["schedule"]["shift_type_id"] ?? 0,
                 "status" => "M",
+                "total_hrs" => '---',
+                "over_stay" => '---',
             ];
 
 
