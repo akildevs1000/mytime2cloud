fbb0ac0402c8a79861c2d2d900ca67720ef4c170
akildevs1000 <107389124+akildevs1000@users.noreply.github.com>
2023-11-30 17:20:25 +0400

    added

--- backend/app/Http/Controllers/ScheduleEmployeeController.php
+++ backend/app/Http/Controllers/ScheduleEmployeeController.php
@@ -82,9 +82,9 @@ class ScheduleEmployeeController extends Controller
             if ($item) {
                 foreach ($data["schedules"] as $shift) {
                     $value = [
-                        "isAutoShift" => $shift["isAutoShift"] ? 1 : 0,
-                        "shift_id" => $shift["isAutoShift"] ? 0 :  $shift["shift_id"],
-                        "shift_type_id" => $shift["isAutoShift"] ? 0 : $shift["shift_type_id"],
+                        "isAutoShift" => array_key_exists("isAutoShift", $shift) && $shift["isAutoShift"] ? 1 : 0,
+                        "shift_id" => array_key_exists("isAutoShift", $shift) && $shift["isAutoShift"] ? 0 :  $shift["shift_id"],
+                        "shift_type_id" => array_key_exists("isAutoShift", $shift) && $shift["isAutoShift"] ? 0 : $shift["shift_type_id"],
                         "isOverTime" => $shift["is_over_time"],
                         "employee_id" => $item,
                         "from_date" => $shift["from_date"],
