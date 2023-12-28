09525577eb589576cb7162497028ce97fe75c5dc
Venu J <akildevs1004@example.com>
2023-12-28 20:29:44 +0400

    kernal

--- backend/app/Console/Kernel.php
+++ backend/app/Console/Kernel.php
@@ -233,11 +233,16 @@ class Kernel extends ConsoleKernel
 
         $schedule->call(function () {
             exec('chown -R www-data:www-data /var/www/mytime2cloud/backend');
-            exec('php artisan cache:clear');
+
             //Artisan::call('cache:clear');
             info("Cache cleared successfully at " . date("d-M-y H:i:s"));
         })->everyThirtyMinutes();
+        $schedule->call(function () {
 
+            exec('php artisan cache:clear');
+            //Artisan::call('cache:clear');
+            info("Cache cleared successfully at " . date("d-M-y H:i:s"));
+        })->everyFifteenMinutes();
 
 
         $schedule
