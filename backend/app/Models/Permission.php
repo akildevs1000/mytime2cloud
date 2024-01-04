c85e25170dc3983e9ece736c9946df5d9edd21a7
Venu J <akildevs1004@example.com>
2024-01-04 19:02:56 +0400

    permission seeder

--- backend/app/Models/Permission.php
+++ backend/app/Models/Permission.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class Permission extends Model
 {
@@ -16,5 +17,13 @@ class Permission extends Model
     public function getTitleAttribute()
     {
         return Str::replace('_', ' ', $this->name);
+    }
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope('order', function (Builder $builder) {
+            $builder->orderBy('name', 'asc');
+        });
     }
 }
