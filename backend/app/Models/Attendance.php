3f1d6ece4f13e00d9a47164653c351c51295f689
akildevs1000 <107389124+akildevs1000@users.noreply.github.com>
2024-01-04 15:39:44 +0400

    added

--- backend/app/Models/Attendance.php
+++ backend/app/Models/Attendance.php
@@ -239,7 +239,7 @@ class Attendance extends Model
             'employee' => function ($q) use ($request) {
                 $q->where('company_id', $request->company_id);
                 $q->where('status', 1);
-                $q->select('system_user_id', 'display_name', "department_id", "first_name", "last_name", "profile_picture", "employee_id", "branch_id");
+                $q->select('system_user_id', 'full_name', 'display_name', "department_id", "first_name", "last_name", "profile_picture", "employee_id", "branch_id");
                 $q->with(['department', 'branch']);
             }
         ]);
