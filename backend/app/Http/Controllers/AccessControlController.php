42fef04f485601f9d9e2a9f78ec5d350a14865ed
akildevs1000 <107389124+akildevs1000@users.noreply.github.com>
2024-01-24 17:13:12 +0400

    added

--- backend/app/Http/Controllers/AccessControlController.php
+++ backend/app/Http/Controllers/AccessControlController.php
@@ -5,8 +5,10 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\AttendanceLog;
 use App\Models\Company;
+use App\Models\Device;
+use App\Models\Employee;
 use Barryvdh\DomPDF\Facade\Pdf;
-
+use Illuminate\Database\Eloquent\Builder;
 
 class AccessControlController extends Controller
 {
@@ -17,7 +19,126 @@ class AccessControlController extends Controller
 
     public function access_control_report_print_pdf(AttendanceLog $model, Request $request)
     {
-        $data = $model->filter($request)->get()->toArray();
+        $model = AttendanceLog::query();
+
+        $model->where("company_id", $request->company_id);
+
+        $model->whereHas('device', fn ($q) => $q->whereIn('device_type', ["all", "Access Control"]));
+
+        $model->when(request()->filled("UserID"), function ($query) use ($request) {
+            return $query->where('UserID', $request->UserID);
+        });
+
+
+        $model->when(request()->filled("DeviceID"), function ($query) use ($request) {
+            return $query->where('DeviceID', $request->DeviceID);
+        });
+
+
+        $model->with("device");
+
+        $model->whereHas("employee", function ($q) use ($request) {
+            $q->where("company_id", $request->company_id);
+            // $q->where('status', 1);
+        });
+
+        $model->with('employee', function ($q) use ($request) {
+            $q->where('company_id', $request->company_id);
+            $q->withOut(["schedule", "department", "sub_department", "designation", "user"]);
+
+            $q->select(
+                "first_name",
+                "last_name",
+                "profile_picture",
+                "employee_id",
+                "branch_id",
+                "system_user_id",
+                "display_name",
+                "timezone_id",
+            );
+        })
+            // ->distinct("LogTime", "UserID", "company_id")
+            ->when($request->filled('department_ids'), function ($q) use ($request) {
+                $q->whereHas('employee', fn (Builder $query) => $query->where('department_id', $request->department_ids));
+            })
+
+            ->with('device', function ($q) use ($request) {
+                $q->where('company_id', $request->company_id);
+            })
+            ->when($request->from_date, function ($query) use ($request) {
+                return $query->where('LogTime', '>=', $request->from_date);
+            })
+            ->when($request->to_date, function ($query) use ($request) {
+                return $query->where('LogTime', '<=', $request->to_date);
+            })
+
+            ->when($request->filled('dates') && count($request->dates) > 1, function ($q) use ($request) {
+                $q->where(function ($query) use ($request) {
+                    $query->where('LogTime', '>=', $request->dates[0])
+                        ->where('LogTime', '<=',   date("Y-m-d", strtotime($request->dates[1] . " +1 day")));
+                });
+            })
+
+            ->when($request->filled('dates') && count($request->dates) > 1, function ($q) use ($request) {
+                $q->where(function ($query) use ($request) {
+                    $query->where('LogTime', '>=', $request->dates[0])
+                        ->where('LogTime', '<=',   date("Y-m-d", strtotime($request->dates[1] . " +1 day")));
+                });
+            })
+
+
+            ->when($request->filled('department'), function ($q) use ($request) {
+
+                $q->whereHas('employee', fn (Builder $query) => $query->where('department_id', $request->department));
+            })
+            ->when($request->filled('LogTime'), function ($q) use ($request) {
+
+                $q->where('LogTime', 'LIKE', "$request->LogTime%");
+            })
+            ->when($request->filled('device'), function ($q) use ($request) {
+                $q->where('DeviceID', $request->device);
+            })
+            ->when($request->filled('system_user_id'), function ($q) use ($request) {
+                $q->where('UserID', $request->system_user_id);
+            })
+            ->when($request->filled('devicelocation'), function ($q) use ($request) {
+                if ($request->devicelocation != 'All Locations') {
+
+                    $q->whereHas('device', fn (Builder $query) => $query->where('location', 'ILIKE', "$request->devicelocation%"));
+                }
+            })
+            ->when($request->filled('employee_first_name'), function ($q) use ($request) {
+                $key = strtolower($request->employee_first_name);
+                $q->whereHas('employee', fn (Builder $query) => $query->where('first_name', 'ILIKE', "$key%"));
+            })
+            ->when($request->filled('branch_id'), function ($q) {
+                $q->whereHas('employee', fn (Builder $query) => $query->where('branch_id', request("branch_id")));
+            })
+
+            ->when(
+                $request->filled('sortBy'),
+                function ($q) use ($request) {
+                    $sortDesc = $request->input('sortDesc');
+                    if (strpos($request->sortBy, '.')) {
+                        if ($request->sortBy == 'employee.first_name') {
+                            $q->orderBy(Employee::select("first_name")->where("company_id", $request->company_id)->whereColumn("employees.system_user_id", "attendance_logs.UserID"), $sortDesc == 'true' ? 'desc' : 'asc');
+                        } else if ($request->sortBy == 'device.name') {
+                            $q->orderBy(Device::select("name")->where("company_id", $request->company_id)->whereColumn("devices.device_id", "attendance_logs.DeviceID"), $sortDesc == 'true' ? 'desc' : 'asc');
+                        } else if ($request->sortBy == 'device.location') {
+                            $q->orderBy(Device::select("location")->where("company_id", $request->company_id)->whereColumn("devices.device_id", "attendance_logs.DeviceID"), $sortDesc == 'true' ? 'desc' : 'asc');
+                        }
+                    } else {
+                        $q->orderBy($request->sortBy . "", $sortDesc == 'true' ? 'desc' : 'asc'); {
+                        }
+                    }
+                }
+            );
+        if (!$request->sortBy) {
+            $model->orderBy('LogTime', 'DESC');
+        }
+
+
+        $data = $model->get()->toArray();
 
         if ($request->debug) return $data;
 
