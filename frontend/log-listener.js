b925d026c3fca083864e63d52a3ab63c88dfddd6
akildevs1000 <107389124+akildevs1000@users.noreply.github.com>
2024-02-28 12:26:22 +0400

    Update log-listener.js

--- frontend/log-listener.js
+++ frontend/log-listener.js
@@ -1,20 +1,7 @@
 const WebSocket = require("ws");
 const fs = require("fs");
-const axios = require('axios');
-const { Agent } = require('https');
-
+const { log } = require("console");
 require("dotenv").config();
-
-const options = {
-  year: "numeric",
-  month: "2-digit",
-  day: "2-digit",
-  hour: "2-digit",
-  minute: "2-digit",
-  second: "2-digit",
-  hour12: false, // Use 24-hour format
-  timeZone: "Asia/Dubai",
-};
 
 const verification_methods = {
   1: "Card",
@@ -47,66 +34,38 @@ const reasons = {
   27: "No Fingerprint",
 };
 
-const [newDate, newTime] = new Intl.DateTimeFormat("en-US", options)
-  .format(new Date())
-  .split(",");
-const [m, d, y] = newDate.split("/");
-const formattedDate = `${d.padStart(2, 0)}-${m.padStart(2, 0)}-${y}`;
-const logFilePath = `../backend/storage/app/logs-${formattedDate}.csv`;
-// const logFilePathRawData = `../backend/storage/app/logs-data/logs-data-${formattedDate}.txt`;
-const logFilePathAlarm = `../backend/storage/app/alarm/alarm-logs-${formattedDate}.csv`;
-console.log(`Current Date: ${formattedDate}`);
-console.log(`Current Time: ${newTime.trim()}`);
-console.log(`logFilePath: ${logFilePath}`);
-
-const { SOCKET_ENDPOINT, WHATSAPP_ENDPOINT, NUMBER, INSTANCE_ID, TOKEN } = process.env;
-
-
-const sendWhatsappMessage = async (msg) => {
-  axios.get(`${WHATSAPP_ENDPOINT}?number=${NUMBER}&type=text&message=${msg}&instance_id=${INSTANCE_ID}&access_token=${TOKEN}`, {
-    httpsAgent: new Agent({
-      rejectUnauthorized: false
-    })
-  })
-    .then(({ data }) => console.log(msg))
-    .catch(error => console.error(error));
-};
-
+const { SOCKET_ENDPOINT } = process.env;
 
 // Create a WebSocket connection
 const socket = new WebSocket(SOCKET_ENDPOINT);
 
-
-let message = `Listener started at ${formattedDate} ${newTime.trim()}.\nReady to listen data from ${SOCKET_ENDPOINT}`;
-console.log(message);
-sendWhatsappMessage(message);
-
 // Handle WebSocket connection events
 socket.onopen = () => {
-  console.log(`Connected to ${SOCKET_ENDPOINT}`);
+  console.log(
+    `Connected to ${SOCKET_ENDPOINT} at ${getFormattedDate().date}${getFormattedDate().time}`
+  );
 };
 
-socket.onerror = async (error) => {
-  let message = "WebSocket error: " + error.message;
-  console.error(message);
-  await sendWhatsappMessage(message);
-
+socket.onerror = (error) => {
+  console.error(
+    `WebSocket error ${error.message
+    } at ${getFormattedDate().date} ${getFormattedDate().time}`
+  );
 };
 // Handle WebSocket close event
-
-socket.onclose = async (event) => {
-  let message = `WebSocket connection closed with code ${event.code
-    } at ${formattedDate} ${newTime.trim()}`;
-  console.error(message);
-  // await sendWhatsappMessage(message);
+socket.onclose = (event) => {
+  console.error(
+    `WebSocket connection closed with code ${event.code
+    } at ${getFormattedDate().date} ${getFormattedDate().time}`
+  );
 };
 
+
 socket.onmessage = ({ data }) => {
-  // try {
-  //   fs.appendFileSync(logFilePathRawData, data + "\n");
-  // } catch (error) {
-  //   console.error("Error processing message:", error.message);
-  // }
+
+  const logFilePath = `../backend/storage/app/logs-${getFormattedDate().date}.csv`;
+  const logFilePathAlarm = `../backend/storage/app/alarm/alarm-logs-${getFormattedDate().date}.csv`;
+
   try {
     const jsonData = JSON.parse(data).Data;
 
@@ -136,14 +95,37 @@ socket.onmessage = ({ data }) => {
   }
 };
 
-process.on("SIGTERM", async () => {
-  let message = `Prcess killed at ${formattedDate} ${newTime.trim()}`;
-  await sendWhatsappMessage(message);
+// Separate function to format date
+function getFormattedDate() {
+
+  const options = {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: false, // Use 24-hour format
+    // timeZone: "Asia/Dubai",
+  };
+  const [newDate, newTime] = new Intl.DateTimeFormat("en-US", options)
+    .format(new Date())
+    .split(",");
+  const [m, d, y] = newDate.split("/");
+
+  return {
+    date: `${d.padStart(2, 0)}-${m.padStart(2, 0)}-${y}`,
+    time: newTime,
+  };
+}
+
+
+process.on("SIGTERM", () => {
+  console.log(`Prcess killed at ${getFormattedDate().date} ${getFormattedDate().time}`);
   process.exit(0); // Exit the process gracefully
 });
 
-process.on("SIGINT", async () => {
-  let message = `Prcess killed at ${formattedDate} ${newTime.trim()}`;
-  await sendWhatsappMessage(message);
+process.on("SIGINT", () => {
+  console.log(`Prcess killed at ${getFormattedDate().date} ${getFormattedDate().time}`);
   process.exit(0); // Exit the process gracefully
 });
