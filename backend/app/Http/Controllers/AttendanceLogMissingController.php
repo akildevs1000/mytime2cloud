50ba9c16140c3feecfb95bfbf508421be738ac07
Venu J <149071174+akildevs1004@users.noreply.github.com>
2024-03-15 16:30:48 +0400

    Logs

--- backend/app/Http/Controllers/AttendanceLogMissingController.php
+++ backend/app/Http/Controllers/AttendanceLogMissingController.php
@@ -75,7 +75,7 @@ class AttendanceLogMissingController  extends Controller
 
             // if ($indexSerialNumber > 0) {
 
-            $url = env("SDK_URL") . $deviceId . "/GetRecordByIndex";
+            $url = env("SDK_URL") . "/"  . $deviceId . "/GetRecordByIndex";
             //$url =   "https://sdk.mytime2cloud.com/" . $deviceId . "/GetRecordByIndex";
             $data =  [
                 "TransactionType" => 1,
@@ -89,18 +89,18 @@ class AttendanceLogMissingController  extends Controller
             $records = $this->culrmethod($url, $data);
             $records = json_decode($records, true);
 
+            if (isset($records['status']))
+                if ($records['status'] != 200) {
+                    $records['message'];
 
-            if ($records['status'] != 200) {
-                $records['message'];
-
-                return [
-                    "status" => 100,
-                    "message" => "Timeout Error", // $records['message'],
-                    "updated_records" => [],
-                    "total_device_records" => [],
-                    "indexSerialNumber" => $indexSerialNumber,
-                ];
-            }
+                    return [
+                        "status" => 100,
+                        "message" => "Timeout Error", // $records['message'],
+                        "updated_records" => [],
+                        "total_device_records" => [],
+                        "indexSerialNumber" => $indexSerialNumber,
+                    ];
+                }
             $finalResult = [];
             foreach ($records['data'] as $record) {
 
@@ -153,7 +153,7 @@ class AttendanceLogMissingController  extends Controller
                 "indexSerialNumber" => $indexSerialNumber,
             ];
 
-            //return $records;
+            return $records;
         } catch (\Exception $e) {
             return [
                 "status" => 102,
