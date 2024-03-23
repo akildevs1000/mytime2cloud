a9318929508b147af62b9ea1279790b1bcc2120c
Venu J <149071174+akildevs1004@users.noreply.github.com>
2024-03-23 19:19:26 +0400

    APIs

--- backend/routes/test.php
+++ backend/routes/test.php
@@ -24,40 +24,50 @@ use App\Models\ReportNotification;
 use App\Models\Shift;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Log as Logger;
 
-Route::get("pdf-merge-test",[TestController::class,"pdfMergeTest"]);
-
-Route::get('/test/getLogs', function (Request $request) {
-
-
-    return (new DeviceSensorLogsController)->updateCompanyIds();
-    $curl = curl_init();
 
 
 
-    $device_id = $request->device_id;
+Route::get("test111password", function (Request $request) {
+    ///////return (new AttendanceLogController)->storemissing();
 
-    curl_setopt_array($curl, array(
-        CURLOPT_URL => "https://sdk.mytime2cloud.com/$device_id/GetRecord",
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_ENCODING => '',
-        CURLOPT_MAXREDIRS => 10,
-        CURLOPT_TIMEOUT => 0,
-        CURLOPT_FOLLOWLOCATION => true,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => 'POST',
-    ));
-
-    $response = curl_exec($curl);
-
-    curl_close($curl);
-    return  json_decode($response, true);
+    //return Hash::make("AkiL@2211");
 });
+Route::get("pdf-merge-test", [TestController::class, "pdfMergeTest"]);
+
+
+// Route::get('/test/getLogs', function (Request $request) {
+
+
+//     return (new DeviceSensorLogsController)->updateCompanyIds();
+//     $curl = curl_init();
+
+
+
+//     $device_id = $request->device_id;
+
+//     curl_setopt_array($curl, array(
+//         CURLOPT_URL => "https://sdk.mytime2cloud.com/$device_id/GetRecord",
+//         CURLOPT_RETURNTRANSFER => true,
+//         CURLOPT_ENCODING => '',
+//         CURLOPT_MAXREDIRS => 10,
+//         CURLOPT_TIMEOUT => 0,
+//         CURLOPT_FOLLOWLOCATION => true,
+//         CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+//         CURLOPT_CUSTOMREQUEST => 'POST',
+//     ));
+
+//     $response = curl_exec($curl);
+
+//     curl_close($curl);
+//     return  json_decode($response, true);
+// });
 Route::get('/test/resetLogCount', function (Request $request) {
     $curl = curl_init();
 
