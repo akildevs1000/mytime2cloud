4ad0098249ef601b521f830bc50aa9b2631536c0
Venu J <149071174+akildevs1004@users.noreply.github.com>
2024-03-25 12:01:52 +0400

    dev_logs

--- backend/app/Http/Controllers/Controller.php
+++ backend/app/Http/Controllers/Controller.php
@@ -400,7 +400,7 @@ class Controller extends BaseController
 
     public function devLog($file_name, $message): void
     {
-        Storage::append("devlogs/" . $file_name . '-' . date('d-m-Y') . ".log", $message . "\n");
+        Storage::append("dev_logs/" . $file_name . '-' . date('d-m-Y') . ".log", $message . "\n");
     }
 
     public function processImage($folder): string
