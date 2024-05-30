352ec6445a707e9353a1322d36f9d66a4c541303
Venu J <149071174+akildevs1004@users.noreply.github.com>
2024-05-30 11:57:24 +0400

    AccessControlTimeSlots

--- backend/app/Console/Commands/AccessControlTimeSlots.php
+++ backend/app/Console/Commands/AccessControlTimeSlots.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use App\Http\Controllers\SDKController;
 use App\Models\AccessControlTimeSlot;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Storage;
 
 class AccessControlTimeSlots extends Command
 {
@@ -34,6 +35,11 @@ class AccessControlTimeSlots extends Command
 
 
         $result = (new SDKController)->handleCommand($id, $command);
+
+        $date = date("Y-m-d");
+        $file_name_raw = "kernal_logs/$date-device-$command-access.log";
+        Storage::append($file_name_raw,  date("d-m-Y H:i:s") . '_door_$command_logs' - ' . $command');
+
 
         return $this->info($result);
     }
