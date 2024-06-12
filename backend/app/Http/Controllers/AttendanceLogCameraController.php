09c8ad7ae98ff86a7dded6b04e64dfa753c47405
Venu J <149071174+akildevs1004@users.noreply.github.com>
2024-06-12 19:17:15 +0400

    log_type

--- backend/app/Http/Controllers/AttendanceLogCameraController.php
+++ backend/app/Http/Controllers/AttendanceLogCameraController.php
@@ -91,7 +91,7 @@ class AttendanceLogCameraController extends Controller
                 "DeviceID" => $columns[1],
                 "LogTime" => substr(str_replace("T", " ", $columns[2]), 0, -3),
                 "SerialNumber" => $columns[3],
-                "log_type" => $columns[4]
+                "log_type" => $columns[4] != '' ?? null
             ];
         }
 
