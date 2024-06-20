f40a94d54006ed501b6df322127a6d43a096f4eb
Venu J <149071174+akildevs1004@users.noreply.github.com>
2024-06-20 18:05:34 +0400

    user.company_id

--- frontend/plugins/axios.js
+++ frontend/plugins/axios.js
@@ -1,10 +1,13 @@
 export default ({ $axios, store }, inject) => {
   $axios.onRequest(async (config) => {
     let user = store.state.auth.user;
-    // config.params = {
-    //   ...config.params,
-    //   company_id: user.company_id,
-    // };
+
+    if (user) {
+      config.params = {
+        ...config.params,
+        company_id: user.company_id,
+      };
+    }
 
     if (user?.role?.role_type == "guard") {
       if (user && user.employee && user.employee.branch_id > 0) {
