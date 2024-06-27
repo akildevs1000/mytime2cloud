5f92e6ff6e26bb426e0d8b76741eeafad0a257c7
Venu J <149071174+akildevs1004@users.noreply.github.com>
2024-06-27 20:07:28 +0400

    kernal_logs

--- backend/app/Http/Controllers/DeviceController.php
+++ backend/app/Http/Controllers/DeviceController.php
@@ -792,7 +792,7 @@ class DeviceController extends Controller
                                 ->command("task:AccessControlTimeSlots {$device["devices"]->device_id} HoldDoor")
                                 ->cron($timeArray[1] . ' ' . $timeArray[0] . ' * * *')
                                 ->withoutOverlapping()
-                                ->appendOutputTo(storage_path("logs/$date-device-access-control-time-slot-open-logs.log"))
+                                ->appendOutputTo(storage_path("kernal_logs/$date-device-access-control-time-slot-open-logs.log"))
                                 ->emailOutputOnFailure(env("ADMIN_MAIL_RECEIVERS"));
                         }
                     }
@@ -830,7 +830,7 @@ class DeviceController extends Controller
                                 ->command("task:AccessControlTimeSlots {$device["devices"]->device_id} CloseDoor")
                                 ->cron($timeArray[1] . ' ' . $timeArray[0] . ' * * *')
                                 ->withoutOverlapping()
-                                ->appendOutputTo(storage_path("logs/$date-device-access-control-time-slot-open-logs.log"))
+                                ->appendOutputTo(storage_path("kernal_logs/$date-device-access-control-time-slot-open-logs.log"))
                                 ->emailOutputOnFailure(env("ADMIN_MAIL_RECEIVERS"));
                         }
                     }
