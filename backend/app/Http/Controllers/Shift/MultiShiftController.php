1ea8fa8ec05636821b295697b8d94e47e2026ee6
Venu J <149071174+akildevs1004@users.noreply.github.com>
2024-08-08 18:40:30 +0400

    getLogsWithInRangeNew

--- backend/app/Http/Controllers/Shift/MultiShiftController.php
+++ backend/app/Http/Controllers/Shift/MultiShiftController.php
@@ -100,9 +100,6 @@ class MultiShiftController extends Controller
 
             $logs = (new AttendanceLog)->getLogsWithInRangeNew($params);
 
-            //  $logs = (new AttendanceLog)->getLogsForRender($params);
-
-
 
 
             $data = $logs[$row->system_user_id] ?? [];
