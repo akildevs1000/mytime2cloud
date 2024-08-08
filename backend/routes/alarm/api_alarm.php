a60fc4ae321fc59c6480e4e134a06680c95419cf
Venu J <149071174+akildevs1004@users.noreply.github.com>
2024-08-08 19:25:58 +0400

    loadalarm_csv

--- backend/routes/alarm/api_alarm.php
+++ backend/routes/alarm/api_alarm.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\Http\Controllers\Alarm\Api\ApiAlarmControlController;
+use App\Http\Controllers\AlarmLogsController;
 use App\Http\Controllers\AnnouncementController;
 use App\Http\Controllers\AnnouncementsCategoriesController;
+use AWS\CRT\HTTP\Request;
 use Illuminate\Support\Facades\Route;
 
 // announcement
@@ -11,3 +13,10 @@ use Illuminate\Support\Facades\Route;
 // Route::get('announcement/search/{key}', [AnnouncementController::class, 'search']);
 //Route::get('alarm_device_status', [ApiAlarmControlController::class, 'LogDeviceStatus']);
 //Route::get('announcement/employee/{id}', [AnnouncementController::class, 'getAnnouncement']);
+//Route::get('announcement/employee/{id}', [AnnouncementController::class, 'getAnnouncement']);
+
+
+Route::get('/loadalarm_csv', function (Request $request) {
+
+    (new AlarmLogsController)->store();
+});
