055899d081624fcbb367968d04da862be49ea9bf
Venu J <149071174+akildevs1004@users.noreply.github.com>
2024-11-28 16:16:01 +0400

    Whatsapp

--- backend/app/Http/Controllers/API/SharjahUniversityAPI.php
+++ backend/app/Http/Controllers/API/SharjahUniversityAPI.php
@@ -30,12 +30,14 @@ class SharjahUniversityAPI extends Controller
             foreach ($attendanceArray as $key => $attendance) {
                 //Storage::append($logFile, date("Y-m-d H:i:s") . ' company_id :' . $attendance['company_id'] . PHP_EOL);
                 try {
+
+                    try {
+                        (new WhatsappNotificationsLogController())->addAttendanceMessageEmployeeId($attendance);
+                    } catch (\Throwable $e) {
+                    }
                     if ($attendance['company_id'] == 13) {
 
-                        // try {
-                        //     (new WhatsappNotificationsLogController())->addAttendanceMessageEmployeeId($attendance);
-                        // } catch (\Throwable $e) {
-                        // }
+
 
                         $data = collect($attendance)->only([
                             'employee_id',
