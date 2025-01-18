7377d79463d8e70ec63e61ce9296c7f4aad220c8
Venu J <149071174+akildevs1004@users.noreply.github.com>
2025-01-18 12:47:33 +0400

    device_healthcheck

--- backend/app/Http/Controllers/AttendanceLogMissingController.php
+++ backend/app/Http/Controllers/AttendanceLogMissingController.php
@@ -56,6 +56,8 @@ class AttendanceLogMissingController  extends Controller
             $source_info = $request->device_healthcheck ?? '';
 
             $source_info .= $request->company_id == 0 ?  'Master_' : $request->company_id;
+            $source_info .= $request->source   ??  '-';
+
             $source_info .= "_missing_logs_" . date("Y-m-d H:i:s");
             $total_records = 0;
             //$deviceId = "FC-8300T20094123";
