182e28f18300bd1ec247442df88f28f42568fbc7
akildevs1000 <akildevs1000@gmail.com>
2025-01-22 20:38:34 +0400

    test

--- backend/app/Console/Commands/AlertAccessControl.php
+++ backend/app/Console/Commands/AlertAccessControl.php
@@ -54,8 +54,8 @@ class AlertAccessControl extends Command
             ->where("LogTime", ">=", date("Y-m-d 00:00:00"))
             ->where("LogTime", "<=", date("Y-m-d 23:59:00"))
             ->where('company_id', $company_id)
-            ->where('is_notified_by_whatsapp_proxy', false)
-            ->limit(5)
+            ->where('is_notified_by_whatsapp_proxy', true)
+            ->limit(1)
             ->orderBy("id", "asc")
             ->get();
 
