f3914434a479b8386a48a250b6349690258443bf
akildevs1000 <akildevs1000@gmail.com>
2025-01-22 13:32:39 +0400

    test

--- backend/app/Console/Commands/AlertAccessControl.php
+++ backend/app/Console/Commands/AlertAccessControl.php
@@ -33,50 +33,19 @@ class AlertAccessControl extends Command
 
         $logger->logOutPut($logFilePath, "*****Cron started for alert:access_control $company_id *****");
 
-        // Fetch the ReportNotification model with filtered managers
-        $model = ReportNotification::with(["managers"])->where('type', 'access_control')->first();
-
-        $this->info($model);
-
-        die;
-
-        // Check if the ReportNotification model exists
-        if (!$model) {
-            $logger->logOutPut($logFilePath, "No Report Notification found for the specified type");
-            $this->info("No ReportNotification found for the specified type.");
-            $logger->logOutPut($logFilePath, "*****Cron ended for alert:access_control $company_id *****");
-            return;
-        }
-
-        $days = $model->days;
-
-        sort($days); // ["0","1","3","4","5","6"]
-
-        $currentDay = date("w"); // day value as number
-        if (!in_array($currentDay, $days) || !count($model->days)) {
-            $logger->logOutPut($logFilePath, "Day not found");
-            $logger->logOutPut($logFilePath, "*****Cron ended for alert:access_control $company_id *****");
-            $this->info("Day not found");
-            return;
-        }
-
-        $from_time = $model->from_time;
-        $to_time = $model->to_time;
-
-        // Extract managers or set an empty array if null
-        $managers = $model->managers ?? [];
-
-        $this->info($managers);
-
-        // Check if there are no managers
-        if ($managers->isEmpty()) {
-            $logger->logOutPut($logFilePath, "No managers found for the specified company ID.");
-            $this->info("No managers found for the specified company ID.");
-            $logger->logOutPut($logFilePath, "*****Cron ended for alert:access_control $company_id *****");
-            return;
-        }
-
         $clientId = Company::where("id", $company_id)->value("company_code") ?? 0;
+
+        $models = ReportNotification::with("managers")
+            ->where('type', 'access_control')
+            ->orderBy("id", "desc")
+            ->get();
+
+        if ($models->isEmpty()) {
+            $logger->logOutPut($logFilePath, "No Report Notification found.");
+            $this->info("No ReportNotification found.");
+            $logger->logOutPut($logFilePath, "*****Cron ended for alert:access_control $company_id *****");
+            return;
+        }
 
         $records = AttendanceLog::with(['employee', 'company', 'device'])
             ->with(["employee" => function ($q) use ($company_id) {
@@ -96,53 +65,76 @@ class AlertAccessControl extends Command
             $logger->logOutPut($logFilePath, "*****Cron ended for alert:access_control $company_id *****");
         }
 
+        foreach ($models as $model) {
 
-        foreach ($records as $key => $record) {
+            $days = $model->days;
+            $from_time = $model->from_time;
+            $to_time = $model->to_time;
+            $managers = $model->managers ?? [];
 
-            $time = $record->time;
+            $currentDay = date("w"); // day value as number
+            if (!in_array($currentDay, $days) || !count($days)) {
+                $logger->logOutPut($logFilePath, "Day not found");
+                $logger->logOutPut($logFilePath, "*****Cron ended for alert:access_control $company_id *****");
+                $this->info("Day not found");
+                return;
+            }
 
-            if ($record->company && $record->employee && $record->device) {
-                if (
-                    ($time >= $from_time && $time <= "23:59") || // Time is on the same day between from_time and midnight
-                    ($time >= "00:00" && $time <= $to_time)      // Time is on the next day between midnight and to_time
-                ) {
-                    try {
-                        $name = ucfirst($record->employee->first_name) . " " . ucfirst($record->employee->last_name);
-                        $formattedDate = (new DateTime($record->LogTime))->format('jS M Y H:i');
-                        $message = $this->generateMessage($name, $record->device->name, $formattedDate);
+            // Check if there are no managers
+            if ($managers->isEmpty()) {
+                $logger->logOutPut($logFilePath, "No managers found for the specified company ID.");
+                $this->info("No managers found for the specified company ID.");
+                $logger->logOutPut($logFilePath, "*****Cron ended for alert:access_control $company_id *****");
+                return;
+            }
 
-                        foreach ($managers as $manager) {
+            foreach ($records as $key => $record) {
 
-                            if (in_array("Whatsapp", $model->mediums)) {
-                                $response = Http::withoutVerifying()->post(
-                                    'https://wa.mytime2cloud.com/send-message',
-                                    [
-                                        'clientId' =>  $clientId,
-                                        'recipient' => $manager->whatsapp_number,
-                                        'text' => $message,
-                                    ]
-                                );
+                $time = $record->time;
+
+                if ($record->company && $record->employee && $record->device) {
+                    if (
+                        ($time >= $from_time && $time <= "23:59") || // Time is on the same day between from_time and midnight
+                        ($time >= "00:00" && $time <= $to_time)      // Time is on the next day between midnight and to_time
+                    ) {
+                        try {
+                            $name = ucfirst($record->employee->first_name) . " " . ucfirst($record->employee->last_name);
+                            $formattedDate = (new DateTime($record->LogTime))->format('jS M Y H:i');
+                            $message = $this->generateMessage($name, $record->device->name, $formattedDate);
+
+                            foreach ($managers as $manager) {
+
+                                if ($manager->branch_id == $record->employee->branch_id) {
+                                    if (in_array("Whatsapp", $model->mediums)) {
+                                        $response = Http::withoutVerifying()->post(
+                                            'https://wa.mytime2cloud.com/send-message',
+                                            [
+                                                'clientId' =>  $clientId,
+                                                'recipient' => $manager->whatsapp_number,
+                                                'text' => $message,
+                                            ]
+                                        );
+
+                                        // To handle the response
+                                        if ($response->successful()) {
+                                            $logger->logOutPut($logFilePath, "Message sent successfully");
+                                            $this->info("Message sent successfully");
+                                        } else {
+                                            $logger->logOutPut($logFilePath, "Failed to send message");
+                                            $this->info("Failed to send message!");
+                                        }
+                                    }
+
+                                    if (in_array("Email", $model->mediums)) {
+                                        // process for email
+                                    }
+                                    sleep(5);
+                                }
                             }
-
-                            if (in_array("Email", $model->mediums)) {
-                                // process for email
-                            }
-                            sleep(5);
+                        } catch (\Throwable $e) {
+                            $this->info($e);
+                            $logger->logOutPut($logFilePath, "Exception: " . $e->getMessage());
                         }
-
-
-
-                        // To handle the response
-                        if ($response->successful()) {
-                            $logger->logOutPut($logFilePath, "Message sent successfully");
-                            $this->info("Message sent successfully");
-                        } else {
-                            $logger->logOutPut($logFilePath, "Failed to send message");
-                            $this->info("Failed to send message!");
-                        }
-                    } catch (\Throwable $e) {
-                        $this->info($e);
-                        $logger->logOutPut($logFilePath, "Exception: " . $e->getMessage());
                     }
                 }
             }
