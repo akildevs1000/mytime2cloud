0cd367cda10f0f35271366f8b28243aa84221bbb
akildevs1000 <akildevs1000@gmail.com>
2025-02-18 11:29:39 +0400

    test

--- backend/app/Models/AttendanceLog.php
+++ backend/app/Models/AttendanceLog.php
@@ -46,8 +46,7 @@ class AttendanceLog extends Model
 
     public function device()
     {
-        return $this->belongsTo(Device::class, "DeviceID", "device_id");
-        // ->withDefault(["name" => "Manual", "device_id" => "Manual"]);
+        return $this->belongsTo(Device::class, "DeviceID", "device_id")->withDefault(["name" => "Manual", "device_id" => "Manual"]);
     }
     public function company()
     {
