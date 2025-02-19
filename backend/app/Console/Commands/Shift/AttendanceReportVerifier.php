df3156c244bca9c0fff8017ac77ba6556bc6cc84
akildevs1000 <akildevs1000@gmail.com>
2025-02-19 11:24:10 +0400

    test

--- backend/app/Console/Commands/Shift/AttendanceReportVerifier.php
+++ backend/app/Console/Commands/Shift/AttendanceReportVerifier.php
@@ -7,14 +7,14 @@ use App\Models\AttendanceLog;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Http;
 
-class FetchAttendanceReport extends Command
+class AttendanceReportVerifier extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'attendance:checker';
+    protected $signature = 'attendance:report_verifier';
 
     /**
      * The console command description.
