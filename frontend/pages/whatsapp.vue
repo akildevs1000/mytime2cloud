5948b9281b4b0e5c32ff78e7adb18b02fe20f692
akildevs1000 <akildevs1000@gmail.com>
2025-02-20 12:05:31 +0400

    added

--- frontend/pages/whatsapp.vue
+++ frontend/pages/whatsapp.vue
@@ -1,115 +1,22 @@
 <template>
-  <v-app>
-    <v-dialog persistent v-model="dialog" width="900">
-      <!-- <AssetsIconClose
-        left="890"
-        @click="
-          () => {
-            dialogKey++;
-            dialog = false;
-          }
-        "
-      /> -->
+  <v-card elevation="0">
+    <v-card-text class="mt-4">
+      <div v-if="statusMessage">
+        <v-badge dense :color="statusColor" offset-y="5" class="mr-5"></v-badge
+        ><span>{{ statusMessage }}</span>
+      </div>
+      <div v-if="qrCodeDisplay == true">
+        <v-img :src="qrCodePath" max-width="200" max-height="200"></v-img>
+      </div>
+   
+      <div>
+        <v-btn class="pa-2" x-small color="primary" @click="connect">
+          <v-icon small class="mr-1">mdi-whatsapp</v-icon>
+          {{ statusMessage !== "Online" ? "Connect" : "Check Online" }}
+        </v-btn>
+      </div>
 
-      <template v-slot:activator="{ on, attr }">
-        <v-row align="center">
-          <v-col cols="12" class="text-center">
-            <v-btn small class="primary" v-bind="attr" v-on="on">
-              <v-icon small> mdi-plus</v-icon> Account
-            </v-btn>
-          </v-col>
-        </v-row>
-      </template>
-
-      <v-container class="white">
-        <style scoped>
-          @keyframes rotate {
-            0% {
-              transform: rotate(360deg);
-            }
-            100% {
-              transform: rotate(0deg);
-            }
-          }
-
-          .rotate {
-            animation: rotate 2s linear infinite;
-          }
-        </style>
-        <v-row class="mb-3">
-          <!----- <v-col cols="12" v-if="qrImage">
-          <v-img :src="qrImage"></v-img>
-        </v-col>
-        ----->
-          <!-- <v-col cols="4">
-          <v-text-field
-            label="Server URL"
-            v-model="serverUrl"
-            outlined
-            dense
-            hide-details
-          />
-        </v-col>
-        <v-col>
-          <v-btn v-if="isConnected" color="red" dark @click="disconnect"
-            >Disonnect</v-btn
-          >
-          <v-btn
-            v-else
-            :loading="loading"
-            color="primary"
-            @click="connectToWebSocket"
-            >Connect</v-btn
-          >
-        </v-col> -->
-
-          <v-col cols="3">
-            <v-row>
-              <v-col cols="12">
-                <div class="headline mb-5">
-                  Whatsapp Proxy
-                  <!-- {{ dialogKey }} {{ clientId }} -->
-                </div>
-
-                <v-avatar v-if="qrCodeUrl" size="200" tile>
-                  <v-img :src="qrCodeUrl" />
-                </v-avatar>
-              </v-col>
-              <v-col cols="12" v-if="statusMessage !== 'Client is ready.'">
-                <v-btn
-                  @click="regenerateClientId"
-                  :loading="regenerateLoading"
-                  small
-                  color="primary"
-                  >Regenerate</v-btn
-                >
-              </v-col>
-
-              <v-col cols="12">
-                <div class="white ma-2" dense>
-                  <v-badge
-                    x-small
-                    dense
-                    hide-details
-                    :color="statusColor"
-                    class="mr-5"
-                  ></v-badge>
-                  <span>{{ statusMessage }}</span>
-
-                  <!-- i want to rorate this v-con  -->
-                  <v-icon
-                    v-if="loading && statusMessage !== 'Client is ready.'"
-                    class="rotate"
-                    color="primary"
-                    >mdi-sync</v-icon
-                  >
-                </div>
-              </v-col>
-            </v-row>
-          </v-col>
-          <v-col>
-            <v-card>
-              <v-container class="px-5">
+      <v-container class="px-0">
                 <h3 class="white">API Usage Example</h3>
                 <p class="black white--text pa-2 my-3">
                   <strong>Endpoint:</strong>
@@ -119,8 +26,8 @@
                 <pre class="black white--text pa-2 mb-3">
   {
     "clientId": "{{ clientId }}",
-    "phone": "971xxxxxxxxx",
-    "message": "test message"
+    "recipient": "971xxxxxxxxx",
+    "text": "test message"
   }
   </pre
                 >
@@ -128,151 +35,87 @@
                 <pre class="black white--text pa-2 mb-3">
   {
     "success": true,
-    "message": "Message sent successfully!"
+    "data": "Message to 971554501483 is being processed."
   }
   </pre
                 >
               </v-container>
-            </v-card>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-dialog>
-  </v-app>
+    </v-card-text>
+  </v-card>
 </template>
 
 <script>
-// import AssetsIconClose from "../components/Widgets/Close.vue";
+import QRCode from "qrcode";
 
 export default {
-  layout: "master",
-  components: {
-    //AssetsIconClose,
-  },
-  head: () => ({
-    title: "Whatsapp Proxy",
-  }),
   data() {
     return {
-      dialogKey: 1,
-      dialog: false,
-      regenerateLoading: false,
-      loading: true,
+      clientId:
+        process.env.WHATSAPP_PROXY_CLIENT_SECRET || "default_client_secret",
       ws: null, // WebSocket instance
-      clientId: "",
-      serverUrl: "wss://wa.mytime2cloud.com/ws/",
-      // serverUrl: "ws://139.59.69.241:5175",
-
+      statusMessage: null, // Message for status updates
+      qrCodePath: null, // QR code image path
+      qrCodeDisplay: false,
+      connectButton: false,
+      statusColor: null,
       endpoint: "wa.mytime2cloud.com/api/send-message",
-      //   endpoint: "http://localhost:5176/api/send-message",
-      qrCodeUrl: null,
-      statusMessage: "",
-      statusColor: "",
-      loading: false,
-      isConnected: false,
-
-      qrImage: null,
-      isRegenerate: true,
     };
   },
   async mounted() {
-    // // Initialize clientId to an empty string
-    // this.clientId = "";
-    // // Check if a clientId exists in localStorage
-    // let clientId = localStorage.getItem("clientId");
-    // clientId = `client_${Math.random().toString(36).substr(2, 9)}`;
-    // localStorage.setItem("clientId", clientId);
-    // this.connectToWebSocket(clientId);
-    // console.log("🚀 ~ mounted ~ mounted:", this.dialogKey);
+    // await this.connect();
   },
   methods: {
-    regenerateClientId() {
-      this.regenerateLoading = true;
-      let clientId = `client_${Math.random().toString(36).substr(2, 9)}`;
-      localStorage.setItem("clientId", clientId);
-      this.clientId = clientId;
-
-      setTimeout(() => {
-        this.connectToWebSocket(clientId);
-        this.regenerateLoading = false;
-      }, 3000);
-    },
-    disconnect() {
-      if (this.ws && this.ws.readyState === WebSocket.OPEN) {
-        // Notify the server to disconnect the WhatsApp client
-        this.ws.send(
-          JSON.stringify({
-            type: "disconnect",
-            clientId: localStorage.getItem("clientId"), // Pass the unique clientId
-          })
-        );
-
-        // Update the UI to reflect the disconnection
-        this.statusMessage = "Disconnected from WhatsApp.";
-        this.statusColor = "warning";
-        this.qrCodeUrl = null;
-
-        localStorage.removeItem("clientId");
-
-        console.log("WhatsApp client disconnected.");
-      } else {
-        console.warn("WebSocket is not open or already disconnected.");
-        this.statusMessage =
-          "No active connection to disconnect from WhatsApp.";
-        this.statusColor = "error";
-      }
+    async connect() {
+      await this.connectWebSocket(this.clientId);
     },
 
-    connectToWebSocket(clientId) {
-      if (!this.serverUrl.trim()) {
-        alert("Please enter a valid WebSocket server URL.");
-        return;
-      }
-
-      this.ws = new WebSocket(`${this.serverUrl}?clientId=${clientId}`);
-      this.loading = true;
+    async connectWebSocket(clientId) {
+      const wsUrl = `wss://wa.mytime2cloud.com/ws/?clientId=${clientId}`;
+      this.ws = new WebSocket(wsUrl);
 
       this.ws.onopen = () => {
-        console.log(`WebSocket connection established with ${clientId}.`);
-        this.ws.send(JSON.stringify({ type: "clientId", clientId }));
+        this.statusMessage = `Connected to the WebSocket server with clientId: ${clientId}`;
       };
 
-      this.ws.onmessage = (event) => {
-        const data = JSON.parse(event.data);
+      this.ws.onmessage = async (event) => {
+        this.qrCodePath = null;
+        this.qrCodeDisplay = false;
 
-        if (data.qr !== undefined) {
-          this.qrCodeUrl = `https://api.qrserver.com/v1/create-qr-code/?size=250x250&data=${encodeURIComponent(
-            data.qr
-          )}`;
-          this.statusMessage = "";
-          this.loading = false;
-          this.statusColor = "";
-          this.isRegenerate = false;
-        } else if (data.type === "status" && data.ready) {
-          this.qrCodeUrl = null;
-          this.statusMessage = data.message;
-          this.loading = true;
+        this.statusMessage = null;
+        this.connectButton = false;
+        this.statusColor = "";
+
+        const data = JSON.parse(event.data);
+        console.log("🚀 ~ this.ws.onmessage= ~ data:", data);
+
+        if (data.event === "status") {
+          this.statusMessage = data.data;
+        } else if (data.event === "ready") {
+          this.statusMessage = data.data;
           this.statusColor = "success";
-          this.isConnected = false;
-          this.clientId = clientId;
-        } else if (data.type === "error") {
-          this.qrCodeUrl = null;
-          this.statusMessage = `Error: ${data.message}`;
-          this.loading = false;
+        } else if (data.event === "qr") {
+          const qrCodeData = data.data;
+          try {
+            // Generate a QR code as a data URL
+            const qrCodePath = await QRCode.toDataURL(qrCodeData, {
+              color: {
+                dark: "#000000", // Black QR code
+                light: "#ffffff", // White background
+              },
+            });
+
+            // Update the path to display the QR code
+            this.qrCodePath = qrCodePath;
+            this.qrCodeDisplay = true;
+          } catch (error) {
+            this.statusMessage = `Error generating QR code: ${error.message}`;
+          }
+        } else if (data.event === "destroy") {
+          this.statusMessage = data.data;
+          this.connectButton = true;
           this.statusColor = "error";
-          this.isRegenerate = true;
         }
       };
-
-      // this.ws.onclose = () => {
-      //   console.log("WebSocket connection closed.");
-      //   this.statusMessage = "Connection lost. Please refresh the page.";
-      //   this.statusColor = "error";
-      //   this.loading = false;
-      // };
-    },
-    reloadPage() {
-      location.reload();
     },
   },
 };
