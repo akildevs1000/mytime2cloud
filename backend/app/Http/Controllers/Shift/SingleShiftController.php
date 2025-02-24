f2d43c10a73dbd14b805dfdbd158771a3b2dd4c0
akildevs1000 <akildevs1000@gmail.com>
2025-02-24 16:08:31 +0400

    test

--- backend/app/Http/Controllers/Shift/SingleShiftController.php
+++ backend/app/Http/Controllers/Shift/SingleShiftController.php
@@ -77,24 +77,24 @@ class SingleShiftController extends Controller
 
 
         //update atendance table with shift ID if shift with employee not found
-        if (count($logsEmployees) == 0) {
-            $employees = (new Employee())->GetEmployeeWithShiftDetails($params);
-            foreach ($employees as $key => $value) {
-                if ($value->schedule->shift && $value->schedule->shift["id"] > 0) {
-                    $data1 = [
-                        "shift_id" => $value->schedule->shift["id"],
-                        "shift_type_id" => $value->schedule->shift["shift_type_id"]
-                    ];
-                    $model1 = Attendance::query();
-                    $model1->whereIn("employee_id", $UserIds);
-                    $model1->where("date", $params["date"]);
-                    $model1->where("shift_id", 0);
-                    $model1->where("shift_type_id", 0);
-                    $model1->where("company_id", $params["company_id"]);
-                    $model1->update($data1);
-                }
-            }
-        }
+        // if (count($logsEmployees) == 0) {
+        //     $employees = (new Employee())->GetEmployeeWithShiftDetails($params);
+        //     foreach ($employees as $key => $value) {
+        //         if ($value->schedule->shift && $value->schedule->shift["id"] > 0) {
+        //             $data1 = [
+        //                 "shift_id" => $value->schedule->shift["id"],
+        //                 "shift_type_id" => $value->schedule->shift["shift_type_id"]
+        //             ];
+        //             $model1 = Attendance::query();
+        //             $model1->whereIn("employee_id", $UserIds);
+        //             $model1->where("date", $params["date"]);
+        //             $model1->where("shift_id", 0);
+        //             $model1->where("shift_type_id", 0);
+        //             $model1->where("company_id", $params["company_id"]);
+        //             $model1->update($data1);
+        //         }
+        //     }
+        // }
 
         $items = [];
 
