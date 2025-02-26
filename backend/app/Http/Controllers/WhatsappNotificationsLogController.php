fe017dd9559422d6587bb9a4e4336f51b906cc33
akildevs1000 <akildevs1000@gmail.com>
2025-02-26 15:56:50 +0400

    test

--- backend/app/Http/Controllers/WhatsappNotificationsLogController.php
+++ backend/app/Http/Controllers/WhatsappNotificationsLogController.php
@@ -144,7 +144,7 @@ class WhatsappNotificationsLogController extends Controller
         if ($company) {
 
             // Whatsapp Proxy
-            $lastClientIdEndpoint = "https://backend.myhotel2cloud/api/get_last_whatsapp_client_id/3";
+            $lastClientIdEndpoint = "https://backend.myhotel2cloud.com/api/get_last_whatsapp_client_id/3";
             $clientIdResponse = Http::withoutVerifying()->get($lastClientIdEndpoint);
             $clientId = $clientIdResponse->json()["clientId"];
 
