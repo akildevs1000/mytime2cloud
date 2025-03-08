ad663eeb1d53da983247ca22d864d53e4dd88fa7
akildevs1000 <akildevs1000@gmail.com>
2025-03-08 17:56:59 +0400

    test

--- backend/routes/developer_logs.php
+++ backend/routes/developer_logs.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/log-view', function () {
 
+    // https://backend.mytime2cloud.com/api/log-view?url=app/logs/whatsapp/22/2025-03-08/17:13.log&app_key=base64:o0274u36dsx5sXvLe4R5XPHPLqFoUr7TMuV5z9lQEy0=
     $app_key = request("app_key");
 
     if ($app_key != env("APP_KEY")) {
@@ -20,5 +21,5 @@ Route::get('/log-view', function () {
         return response()->json(['message' => 'Log file not found'], 404);
     }
 
-    return Response::make(File::get($path), 200);
+    return Response::make(nl2br(File::get($path)), 200);
 });
