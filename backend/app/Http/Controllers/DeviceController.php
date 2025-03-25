de738a0a6d7b77c00f2cd2b3607bb2f3cb7cb96c
akildevs1000 <akildevs1000@gmail.com>
2025-03-25 19:01:37 +0400

    test

--- backend/app/Http/Controllers/DeviceController.php
+++ backend/app/Http/Controllers/DeviceController.php
@@ -410,8 +410,11 @@ class DeviceController extends Controller
 
     public function getLastRecordsHistory($id = 0, $count = 0, Request $request)
     {
+        // Generate a unique cache key using company_id and request parameters
+        $cacheKey = "attendance_logs_{$id}_" . md5(json_encode($request->all()));
 
-        $model = AttendanceLog::query();
+        return Cache::remember($cacheKey, now()->addMinutes(10), function () use ($id, $request) {
+            $model = AttendanceLog::query();
 
             $model->where("company_id", $id);
 
@@ -467,12 +470,6 @@ class DeviceController extends Controller
             $model->orderBy('LogTime', 'DESC');
 
             return $model->paginate(request("per_page", 10));
-            
-        // Generate a unique cache key using company_id and request parameters
-        $cacheKey = "attendance_logs_{$id}_" . md5(json_encode($request->all()));
-
-        return Cache::remember($cacheKey, now()->addMinutes(10), function () use ($id, $request) {
-            
         });
     }
 
