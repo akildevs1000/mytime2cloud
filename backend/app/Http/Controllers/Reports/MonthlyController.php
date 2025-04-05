da0795f8e5524378d50d8f517d3d6c3ca438551e
akildevs1000 <akildevs1000@gmail.com>
2025-04-05 11:42:38 +0400

    tes

--- backend/app/Http/Controllers/Reports/MonthlyController.php
+++ backend/app/Http/Controllers/Reports/MonthlyController.php
@@ -28,6 +28,16 @@ class MonthlyController extends Controller
 
     public function monthly(Request $request)
     {
+        ini_set('memory_limit', '512M');
+        ini_set('max_execution_time', 300); // Increase to 5 minutes
+        
+        $showTabs = json_decode($request->showTabs, true);
+
+        // only for multi in/out
+        if ($showTabs['multi'] == true || $showTabs['dual'] == true) {
+            return $this->multi_in_out_monthly_download_pdf($request);
+        }
+        
         $file_name = "Attendance Report";
         if (isset($request->from_date) && isset($request->to_date)) {
             $file_name = "Attendance Report - " . $request->from_date . ' to ' . $request->to_date;
