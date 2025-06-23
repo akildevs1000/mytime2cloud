96e430d1a1ed0abdda719df40eb00e235e46a608
akildevs1000 <akildevs1000@gmail.com>
2025-06-23 11:08:59 +0400

    added icon and alert bar for theme dark and light mode support

---
+++ frontend/components/Theme/Alert.vue
@@ -0,0 +1,18 @@
+<template>
+  <span>
+    <style scoped>
+      .v-alert.theme--dark {
+        background-color: #696cff !important;
+        color: #fff !important;
+      }
+    </style>
+    <v-alert
+      :class="`${$vuetify.theme.dark ? '' : 'primary white--text'}`"
+      dense
+      flat
+      style="font-weight: 400"
+    >
+      <slot></slot>
+    </v-alert>
+  </span>
+</template>
