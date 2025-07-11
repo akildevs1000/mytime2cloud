4cf50139880522ef40f5594ea9e2a9805e97474c
akildevs1000 <akildevs1000@gmail.com>
2025-07-11 15:05:05 +0400

    test

--- backend/app/Console/Kernel.php
+++ backend/app/Console/Kernel.php
@@ -18,7 +18,7 @@ class Kernel extends ConsoleKernel
     {
 
         $schedule->command('whatsapp:proxy-health-check')
-            ->everyFifteenMinutes()
+            ->hourly()
             ->withoutOverlapping();
 
         $schedule->command('monitor:disk')->dailyAt('08:00');
