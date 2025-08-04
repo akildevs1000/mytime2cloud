a2443238798009e44c9797efd2c2e83938eedefe
akildevs1000 <akildevs1000@gmail.com>
2025-08-04 18:19:12 +0400

    test

--- backend/app/Providers/SlowQueryLoggingServiceProvider.php
+++ backend/app/Providers/SlowQueryLoggingServiceProvider.php
@@ -25,11 +25,13 @@ class SlowQueryLoggingServiceProvider extends ServiceProvider
     public function boot()
     {
         DB::listen(function ($query) {
-            if ($query->time > 500) {
+            if ($query->time > 2000) { // 2000 ms = 2 seconds
                 $backtrace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 10);
 
                 $caller = collect($backtrace)->first(function ($trace) {
-                    return isset($trace['file']) && str_starts_with($trace['file'], app_path());
+                    return isset($trace['file'])
+                    && str_starts_with($trace['file'], app_path())
+                    && str_ends_with($trace['file'], '.php');
                 });
 
                 Log::channel('slowqueries')->warning('⏱️ Slow Query Detected', [
