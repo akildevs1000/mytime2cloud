8d13a854a8d904ff58daf051d5187426aa8701d8
akildevs1000 <akildevs1000@gmail.com>
2025-08-15 16:43:32 +0400

    tets

--- backend/app/Http/Controllers/Reports/MonthlyController.php
+++ backend/app/Http/Controllers/Reports/MonthlyController.php
@@ -135,9 +135,11 @@ class MonthlyController extends Controller
         }
 
         // only for multi in/out
-        if ($showTabs['multi'] == true || $showTabs['dual'] == true) {
-            return $this->PDFMerge("D");
-        }
+        // if ($showTabs['multi'] == true || $showTabs['dual'] == true) {
+        //     return $this->PDFMerge("D");
+        // }
+
+         return $this->PDFMerge("D");
 
         $file_name = "Attendance Report";
         if (isset($request->from_date) && isset($request->to_date)) {
