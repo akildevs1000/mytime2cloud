2d2f2504266ac3a7640724730515b4f6f27a4fcb
akildevs1000 <akildevs1000@gmail.com>
2025-09-18 17:17:58 +0400

    test

--- backend/resources/views/pdf/attendance_reports/Template1-new.blade.php
+++ backend/resources/views/pdf/attendance_reports/Template1-new.blade.php
@@ -154,7 +154,7 @@
                             @endfor
                         @else
                             <td class="text-center" colspan="4" style="font-size:6px; color: {{ $date->device_in->name == 'Manual' ? red : '' }}">{{ $date->in ?? '---' }}</td>
-                            <td class="text-center" colspan="4" style="font-size:6px; color: {{ ($date->device_out->name ?? '') === 'Manual' ? 'red' : '' }}">{{ $date->out ?? '---' }}</td>
+                            <td class="text-center" colspan="4" style="font-size:6px; color: {{ $date->device_out->name == 'Manual' ? red : '' }}">{{ $date->out ?? '---' }}</td>
                             <td class="text-center" colspan="3">{{ $date->late_coming ?? '---' }}</td>
                             <td class="text-center" colspan="3">{{ $date->early_going ?? '---' }}</td>
                         @endif
