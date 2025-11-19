ec4df70a47800896af3e268ec51b8cc1dc1118c6
akildevs1000 <akildevs1000@gmail.com>
2025-11-19 12:06:03 +0400

    test

--- backend/app/Http/Controllers/AttendanceLogController.php
+++ backend/app/Http/Controllers/AttendanceLogController.php
@@ -750,11 +750,11 @@ class AttendanceLogController extends Controller
             $log['gps_location'] = $this->reverseGeocode($log['lat'],  $log['lon']) ?? "----";
         }
 
-        try {
-            DB::table('attendance_logs')->insert($logs);
-        } catch (\Exception $e) {
-            Logger::error("Insert failed: " . $e->getMessage());
-        }
+        // try {
+        //     DB::table('attendance_logs')->insert($logs);
+        // } catch (\Exception $e) {
+        //     Logger::error("Insert failed: " . $e->getMessage());
+        // }
 
         return response()->json([
             'message' => 'Logs inserted successfully',
