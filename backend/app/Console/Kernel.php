9d5828a4d46769e3c7da4ea58ddd4fe9e7336354
akildevs1000 <akildevs1000@gmail.com>
2025-11-25 13:58:20 +0400

    test

--- backend/app/Console/Kernel.php
+++ backend/app/Console/Kernel.php
@@ -122,6 +122,13 @@ class Kernel extends ConsoleKernel
                 ->between('5:00', '23:59')
                 ->runInBackground();
 
+            $schedule->command("task:sync_double_shift {$companyId} " . date("Y-m-d"))
+                ->everyThirtyMinutes()
+                ->between('5:00', '23:59')
+                ->withoutOverlapping()
+                ->runInBackground();
+
+
             $schedule->command("task:sync_multi_shift_dual_day {$companyId} " . date("Y-m-d", strtotime("yesterday")) . " true")
                 // ->everyThirtyMinutes()
                 ->dailyAt('5:20')
