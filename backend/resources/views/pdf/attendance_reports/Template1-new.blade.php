781309f75be14bc90932306d5542e634ddb0929e
akildevs1000 <akildevs1000@gmail.com>
2025-12-03 11:50:59 +0400

    test

--- backend/resources/views/pdf/attendance_reports/Template1-new.blade.php
+++ backend/resources/views/pdf/attendance_reports/Template1-new.blade.php
@@ -918,10 +918,8 @@
                                     <td colspan="4"></td>
                                     <th style="text-align: left;color:#f97316;">{{ $info->total_late ?? 0 }}</th>
                                     <th style="text-align: left;color:#f97316;">{{ $info->total_early ?? 0 }}</th>
-                                @elseif ($shift_type_id == 5)
-                                    <td colspan="14"></td>
                                 @else
-                                    <td colspan="15"></td>
+                                    <td colspan="5"></td>
                                 @endif
                                 <th style="text-align: left">{{ $info->total_hours ?? 0 }}</th>
                                 <th style="text-align: left">{{ $info->total_ot_hours ?? 0 }}</th>
