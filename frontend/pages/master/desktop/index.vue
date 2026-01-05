de3d7dc9531dd6aea5313b35c97eef5f4d88e2f9
akildevs1000 <akildevs1000@gmail.com>
2026-01-05 13:21:43 +0400

    test

--- frontend/pages/master/desktop/index.vue
+++ frontend/pages/master/desktop/index.vue
@@ -4,6 +4,16 @@
 
 <script>
 export default {
+  layout({ $auth }) {
+    let { user_type } = $auth.user;
+    if (user_type == "master") {
+      return "master";
+    } else if (user_type == "employee") {
+      return "employee";
+    } else if (user_type == "master") {
+      return "default";
+    }
+  },
   data: () => ({
     baseUrl: "https://debackend.myhotel2cloud.com/api",
     // baseUrl: "http://localhost:8005/api",
