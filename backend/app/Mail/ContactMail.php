727b2a0c55c26458131a320f381a42ece6bd14bd
akildevs1000 <akildevs1000@gmail.com>
2026-01-30 16:53:31 +0400

    test

--- backend/app/Mail/ContactMail.php
+++ backend/app/Mail/ContactMail.php
@@ -8,7 +8,7 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class ContactMail extends Mailable 
+class ContactMail extends Mailable
 {
     use Queueable, SerializesModels;
 
@@ -19,20 +19,15 @@ class ContactMail extends Mailable
         $this->data = $data;
     }
 
-public function build()
-{
-    // MUST be an email from your server's domain (e.g., info@mytime2cloud.com)
-    return $this->from('info@mytime2cloud.com', 'System Admin') 
-        
-        // This allows you to click "Reply" and talk to the user
-        ->replyTo($this->data['email'], $this->data['name']) 
-        
-        ->subject('New Lead: ' . ($this->data['company'] ?? 'Contact'))
-        ->html("
+    public function build()
+    {
+        // MUST be an email from your server's domain (e.g., info@mytime2cloud.com)
+        return $this->subject('New Lead: ' . ($this->data['company'] ?? 'Contact'))
+            ->html("
             <h3>New Contact Form Submission</h3>
             <p><strong>Name:</strong> {$this->data['name']}</p>
             <p><strong>Email:</strong> {$this->data['email']}</p>
             <p><strong>Message:</strong> {$this->data['message']}</p>
         ");
-}
+    }
 }
