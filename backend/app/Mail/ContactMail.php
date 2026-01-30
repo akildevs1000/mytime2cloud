eb534c503cfdda0e2adad0c1c3417f99f33f13e8
akildevs1000 <akildevs1000@gmail.com>
2026-01-30 15:03:40 +0400

    test

--- backend/app/Mail/ContactMail.php
+++ backend/app/Mail/ContactMail.php
@@ -4,11 +4,11 @@
 namespace App\Mail;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
+// use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class ContactMail extends Mailable implements ShouldQueue
+class ContactMail extends Mailable 
 {
     use Queueable, SerializesModels;
 
