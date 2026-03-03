cb45eda0c80e3d804f4651165c09529ae847ac51
akildevs1000 <107389124+akildevs1000@users.noreply.github.com>
2026-03-04 01:23:29 +0400

    style: Update AttendancePage layout for improved visual consistency and readability

--- frontend-new/src/app/attendance/page.js
+++ frontend-new/src/app/attendance/page.js
@@ -25,20 +25,18 @@ export default function AttendancePage() {
     return (
         <div className="w-full p-4 pb-24 overflow-y-auto max-h-[calc(100vh)]">
             {/* Header and Tab Bar */}
-            <div className="flex flex-col md:flex-row md:items-center justify-between  mb-6">
-                <h1 className="text-xl font-bold text-slate-800 dark:text-white mb-4 md:mb-0">
+            <div className="flex px-10 flex-col md:flex-row md:items-center justify-between  mb-6">
+                <h1 className="text-2xl font-extrabold text-gray-600 dark:text-slate-300 flex items-center">
                 </h1>
-
                 <div className="flex space-x-8">
                     {TABS.map((tab) => (
                         <button
                             key={tab}
                             onClick={() => setActiveTab(tab)}
-                            className={`pb-3 px-1 text-sm font-bold tracking-wide uppercase transition-colors border-b-[3px] ${
-                                activeTab === tab
-                                    ? 'border-[#7f19e6] text-[#7f19e6]'
-                                    : 'border-transparent text-slate-500 dark:text-slate-400 hover:text-slate-800 dark:hover:text-slate-200'
-                            }`}
+                            className={`pb-3 px-1 text-sm font-bold tracking-wide uppercase transition-colors border-b-[3px] ${activeTab === tab
+                                ? 'border-[#7f19e6] text-[#7f19e6]'
+                                : 'border-transparent text-slate-500 dark:text-slate-400 hover:text-slate-800 dark:hover:text-slate-200'
+                                }`}
                         >
                             {tab}
                         </button>
