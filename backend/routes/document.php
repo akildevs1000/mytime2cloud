f9c7fe71de2dbbc3feee93b061f645b1177f1958
akildevs1000 <107389124+akildevs1000@users.noreply.github.com>
2026-03-04 22:52:57 +0400

    Refactor code structure for improved readability and maintainability

--- backend/routes/document.php
+++ backend/routes/document.php
@@ -5,5 +5,4 @@ use App\Http\Controllers\DocumentInfoController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('document', DocumentController::class);
-
-Route::get('document/upcoming-expiry', [DocumentInfoController::class, 'upcomingExpiry']);
+Route::get('document-upcoming-expiry', [DocumentInfoController::class, 'upcomingExpiry']);
