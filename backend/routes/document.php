adfdda5745834c0b88bb81e6d8191035e7b35215
akildevs1000 <107389124+akildevs1000@users.noreply.github.com>
2026-03-04 22:51:31 +0400

    feat: Add route for upcomingExpiry method in DocumentInfoController

--- backend/routes/document.php
+++ backend/routes/document.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\DocumentController;
+use App\Http\Controllers\DocumentInfoController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('document', DocumentController::class);
+
+Route::get('document/upcoming-expiry', [DocumentInfoController::class, 'upcomingExpiry']);
