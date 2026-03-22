897961055ca0b8c02853ab4cf2adb8fc3f2d1256
akildevs1000 <107389124+akildevs1000@users.noreply.github.com>
2026-03-22 15:27:04 +0400

    Add new endpoint for report generation v1 in MonthlyController

--- backend/app/Http/Controllers/Reports/MonthlyController.php
+++ backend/app/Http/Controllers/Reports/MonthlyController.php
@@ -44,66 +44,52 @@ class MonthlyController extends Controller
         ];
 
         $companyId    = $requestPayload["company_id"];
-        $employeeIds = $requestPayload["employee_ids"];
-        $template     = $requestPayload["template"];
-        $fromDate     = $requestPayload["from_date"];
-        $toDate       = $requestPayload["to_date"];
+        $employee_ids = $requestPayload["employee_ids"];
 
-        // 1. Get the employees
-        $employeeIds = Employee::where("company_id", $companyId)
-            ->whereIn("system_user_id", $employeeIds)
-            ->pluck("id")->toArray();
+        $company = Company::whereId($companyId)
+            ->with('contact:id,company_id,number')
+            ->first(["logo", "name", "company_code", "location", "p_o_box_no", "id"]);
 
-        if (empty($employeeIds)) {
-            return response()->json(['status' => 'error', 'message' => 'No employees found.'], 404);
-        }
+        $totalEmployees = Employee::where("company_id", $companyId)
+            ->whereIn("system_user_id", $employee_ids)
+            ->count();
 
+        info($totalEmployees);
+        info($employee_ids);
 
-        $directory = public_path("reports/{$companyId}/{$template}");
+        Cache::put("batch_total", $totalEmployees, 1800);
+        Cache::put("batch_done", 0, 1800);
+        Cache::put("batch_failed", 0, 1800);
 
-        if (!File::isDirectory($directory)) {
-            return response()->json(['error' => "Directory not found: {$directory}"], 404);
-        }
+        Employee::with(["schedule" => function ($q) use ($companyId) {
+            $q->where("company_id", $companyId)
+                ->select("id", "shift_id", "shift_type_id", "company_id", "employee_id")
+                ->withOut(["shift", "shift_type", "branch"]);
+        }])
+            ->withOut(["branch", "designation", "sub_department", "user"])
+            ->where("company_id", $companyId)
+            ->whereIn("system_user_id", $employee_ids)
+            ->chunk(50, function ($employees) use ($company, $requestPayload) {
 
-        $pdfFiles = [];
+                foreach ($employees as $employee) {
 
-        try {
-            if (!empty($employeeIds)) {
-                // Merge specific employees from the array
-                foreach ($employeeIds as $id) {
-                    $fileName = "Attendance_Report_{$template}_{$fromDate}_{$toDate}_{$id}.pdf";
-                    $fullPath = $directory . DIRECTORY_SEPARATOR . $fileName;
-
-                    if (File::exists($fullPath)) {
-                        $pdfFiles[] = $fullPath;
-                    }
+                    GenerateAttendanceReportPDF::dispatch(
+                        $employee->system_user_id,
+                        $company,
+                        $employee,
+                        $requestPayload,
+                        optional($employee->schedule)->shift_type_id ?? 0,
+                        $requestPayload["template"] ?? "Template1"
+                    );
                 }
-            } else {
-                // Merge ALL files in that folder matching the pattern
-                $pattern = "Attendance_Report_{$template}_{$fromDate}_{$toDate}_*.pdf";
-                $pdfFiles = glob($directory . DIRECTORY_SEPARATOR . $pattern);
-            }
 
-            if (empty($pdfFiles)) {
-                return response()->json(['error' => "No PDF files found to merge."], 404);
-            }
+                gc_collect_cycles();
+            });
 
-            // Prepare Output Name
-            $outputFileName = "Attendance_Report_{$fromDate}_to_{$toDate}.pdf";
-            $outputPath = $directory . DIRECTORY_SEPARATOR . $outputFileName;
-
-            // Use "F" to save the file on the server disk
-            $this->mergePdfFiles($pdfFiles, "F", $outputPath);
-
-            // Return the URL so the frontend can download it
-            return response()->json([
-                'status' => 'success',
-                'file_name' => $outputFileName,
-                'download_url' => asset("reports/{$companyId}/{$template}/{$outputFileName}")
-            ]);
-        } catch (Exception $e) {
-            return response()->json(['error' => $e->getMessage()], 500);
-        }
+        return response()->json([
+            'status'  => 'processing',
+            'message' => 'Report generation has started.',
+        ]);
     }
 
     public function monthly(Request $request)
@@ -1175,5 +1161,80 @@ class MonthlyController extends Controller
         }
 
         return response($pdf->Output("report.pdf", $action = "D"))->header('Content-Type', 'application/pdf');
+    }
+
+    public function startReportGenerationV1(Request $request)
+    {
+        $requestPayload = [
+            'company_id'   => $request->company_id,
+            'status'       => "-1",
+            'status_slug'  => (new Controller)->getStatusSlug("-1"),
+            'from_date'    => $request->from_date,
+            'to_date'      => $request->to_date,
+            'employee_ids' => $request->input('employee_id', []),
+            'template'     => $request->input('report_template'),
+        ];
+
+        $companyId    = $requestPayload["company_id"];
+        $employeeIds = $requestPayload["employee_ids"];
+        $template     = $requestPayload["template"];
+        $fromDate     = $requestPayload["from_date"];
+        $toDate       = $requestPayload["to_date"];
+
+        // 1. Get the employees
+        $employeeIds = Employee::where("company_id", $companyId)
+            ->whereIn("system_user_id", $employeeIds)
+            ->pluck("id")->toArray();
+
+        if (empty($employeeIds)) {
+            return response()->json(['status' => 'error', 'message' => 'No employees found.'], 404);
+        }
+
+
+        $directory = public_path("reports/{$companyId}/{$template}");
+
+        if (!File::isDirectory($directory)) {
+            return response()->json(['error' => "Directory not found: {$directory}"], 404);
+        }
+
+        $pdfFiles = [];
+
+        try {
+            if (!empty($employeeIds)) {
+                // Merge specific employees from the array
+                foreach ($employeeIds as $id) {
+                    $fileName = "Attendance_Report_{$template}_{$fromDate}_{$toDate}_{$id}.pdf";
+                    $fullPath = $directory . DIRECTORY_SEPARATOR . $fileName;
+
+                    if (File::exists($fullPath)) {
+                        $pdfFiles[] = $fullPath;
+                    }
+                }
+            } else {
+                // Merge ALL files in that folder matching the pattern
+                $pattern = "Attendance_Report_{$template}_{$fromDate}_{$toDate}_*.pdf";
+                $pdfFiles = glob($directory . DIRECTORY_SEPARATOR . $pattern);
+            }
+
+            if (empty($pdfFiles)) {
+                return response()->json(['error' => "No PDF files found to merge."], 404);
+            }
+
+            // Prepare Output Name
+            $outputFileName = "Attendance_Report_{$fromDate}_to_{$toDate}.pdf";
+            $outputPath = $directory . DIRECTORY_SEPARATOR . $outputFileName;
+
+            // Use "F" to save the file on the server disk
+            $this->mergePdfFiles($pdfFiles, "F", $outputPath);
+
+            // Return the URL so the frontend can download it
+            return response()->json([
+                'status' => 'success',
+                'file_name' => $outputFileName,
+                'download_url' => asset("reports/{$companyId}/{$template}/{$outputFileName}")
+            ]);
+        } catch (Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
     }
 }
