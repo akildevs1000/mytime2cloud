318850a1fb2f67807b038fb99b4a503acd9cba11
akildevs1000 <107389124+akildevs1000@users.noreply.github.com>
2026-03-22 22:44:16 +0400

    test

--- backend/app/Http/Controllers/ScheduleEmployeeController.php
+++ backend/app/Http/Controllers/ScheduleEmployeeController.php
@@ -722,10 +722,11 @@ class ScheduleEmployeeController extends Controller
 
         $totalWorkforce = Employee::where('company_id', $companyId)->count();
 
-        $assignedQueryCount = Shift::where('branch_id', ">", 0)->whereHas('employee_schedule', function ($q) use ($companyId) {
-            $q->where('company_id', $companyId);
-            $q->where('branch_id', ">", 0);
-        })
+        $assignedQueryCount = Shift::where('company_id', $companyId)
+            ->where('branch_id', ">", 0)->whereHas('employee_schedule', function ($q) use ($companyId) {
+                $q->where('company_id', $companyId);
+                $q->where('branch_id', ">", 0);
+            })
             ->distinct()
             ->count('id');
 
