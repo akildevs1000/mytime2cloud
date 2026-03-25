744b94a36d5dd2156efbd951a6212545ae55bd95
akildevs1000 <107389124+akildevs1000@users.noreply.github.com>
2026-03-25 12:30:29 +0400

    testing

--- backend/app/Http/Controllers/Shift/RenderController.php
+++ backend/app/Http/Controllers/Shift/RenderController.php
@@ -100,11 +100,14 @@ class RenderController extends Controller
             }
         }
 
+
+        return (new FiloShiftController)->renderData($request);
+
         return array_merge(
-            (new AutoShiftController)->renderData($request),
+            // (new AutoShiftController)->renderData($request),
             (new FiloShiftController)->renderData($request),
-            (new SingleShiftController)->renderData($request),
-            (new NightShiftController)->renderData($request),
+            // (new SingleShiftController)->renderData($request),
+            // (new NightShiftController)->renderData($request),
         );
     }
 
