ace417859736546c3be770b9d220593a0b3f0c00
akildevs1000 <akildevs1000@gmail.com>
2026-04-07 19:12:17 +0400

    test

--- backend/app/Console/Commands/SyncCompanyShifts.php
+++ backend/app/Console/Commands/SyncCompanyShifts.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use App\Models\Company;
 use App\Traits\LogsTable; // 1. Use the Trait
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Artisan;
 
 class SyncCompanyShifts extends Command
@@ -17,8 +18,9 @@ class SyncCompanyShifts extends Command
     public function handle()
     {
         $today = date("Y-m-d");
+        $yesterday = Carbon::yesterday()->format('Y-m-d');
         $hour = (int)date('H');
-        
+
         $companyIds = Company::whereHas('attendance_logs', function ($query) use ($today) {
             $query->whereDate('log_date', $today);
         })->pluck('id');
@@ -31,13 +33,15 @@ class SyncCompanyShifts extends Command
             // Execute commands
             Artisan::call("task:sync_attendance_missing_shift_ids $id $today");
             Artisan::call("task:sync_auto_shift $id $today");
-            Artisan::call("task:sync_except_auto_shift $id $today");
 
             $multiShift = 'Skipped';
-            if ($hour >= 5) {
+            if ($hour >= 6) {
                 Artisan::call("task:sync_multi_shift_v1 $id $today");
                 Artisan::call("task:sync_split_shift $id $today");
+                Artisan::call("task:sync_except_auto_shift $id $today");
                 $multiShift = 'Synced';
+            } else {
+                Artisan::call("task:sync_except_auto_shift $id $yesterday");
             }
 
             // Collect data for the table (Trait will handle formatting)
