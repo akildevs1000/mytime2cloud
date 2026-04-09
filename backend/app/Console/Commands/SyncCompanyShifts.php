998b253124a3a026d7c0e51de0fd7ee028d01aeb
akildevs1000 <akildevs1000@gmail.com>
2026-04-09 20:03:36 +0400

    test

--- backend/app/Console/Commands/SyncCompanyShifts.php
+++ backend/app/Console/Commands/SyncCompanyShifts.php
@@ -64,7 +64,7 @@ class SyncCompanyShifts extends Command
                 Artisan::call("task:sync_flexible_shift $id $today");
                 $multiShift = "Synced (Today)";
             } else {
-                Artisan::call("task:sync_except_auto_shift $id $yesterday");
+                Artisan::call("task:sync_flexible_shift $id $today");
                 $multiShift = "Synced (Yesterday)";
             }
 
