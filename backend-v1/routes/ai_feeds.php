6824e17a0617ece065d6b0c289de2b280b40b779
akildevs1000 <akildevs1000@gmail.com>
2026-04-11 15:02:55 +0400

    test

--- backend-v1/routes/ai_feeds.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use App\Http\Controllers\AIFeedsController;
-use Illuminate\Support\Facades\Route;
-
-Route::get('ai-feeds', [AIFeedsController::class, 'index']);
-Route::get('ai-feeds/{employee_id}', [AIFeedsController::class, 'aiFeedsByEmployeeId']);
