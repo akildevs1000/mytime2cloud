8e1d42265d65dcca1c3ae5c7253aec6987e1b2f7
akildevs1000 <akildevs1000@gmail.com>
2026-05-01 16:41:36 +0400

    test

--- backend/app/Mail/InvoiceMail.php
+++ backend/app/Mail/InvoiceMail.php
@@ -3,14 +3,10 @@
 namespace App\Mail;
 
 use App\Models\Invoice;
-use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
-use Illuminate\Queue\SerializesModels;
 
-class InvoiceMail extends Mailable implements ShouldQueue
+class InvoiceMail extends Mailable
 {
-    use Queueable, SerializesModels;
 
     public Invoice $invoice;
     public ?string $customMessage;
